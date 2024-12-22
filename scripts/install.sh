374fb4f8d17dbedf30adb97ecb3985203157ac07
Keith Khadar <keith.khadar@gmail.com>
2024-12-22 19:46:56 +0000

    Changed back to editing pip conf

--- scripts/install.sh
+++ scripts/install.sh
@@ -131,11 +131,13 @@ mil_system_install --no-install-recommends \
 	wget \
 	vim
 
-# Turn on breaking system packages 
+# Turn on breaking system packages in pip (set to 0 by default in noble)
 # Generally, our packages shouldn't break the system, but we will continue to monitor
 # this for the future. ROS2 and rosdep have a hard time with virtual environments,
 # and using system pip packages in the past has been fine.
-sudo pip3 install -U vcstool --break-system-packages
+sudo python3 -m pip config set global.break-system-packages true
+
+sudo pip3 install -U vcstool
 
 cat <<EOF
 $(color "$Pur")
@@ -206,7 +208,7 @@ if which update-manager >/dev/null 2>&1; then
 fi
 
 # Install Python 3 dependencies
-sudo pip3 install -r requirements.txt --break-system-packages
+sudo pip3 install -r requirements.txt
 
 cat <<EOF
 $(color "$Pur")
@@ -217,7 +219,7 @@ $(hash_header)$(color "$Res")
 EOF
 
 # Install Colcon
-sudo pip3 install -U colcon-common-extensions --break-system-packages
+sudo pip3 install -U colcon-common-extensions
 
 # Initialize rosdep
 sudo apt-get install -y python3-rosdep
