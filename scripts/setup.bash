02b27fd4fcd89a98c4cb2394f794a0a72c0548de
Keith Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2024-12-22 15:46:16 -0500

    init setup.bash (#4)
    Co-authored-by: Keith Khadar <keithkhadar@gmail.com>

---
+++ scripts/setup.bash
@@ -0,0 +1,24 @@
+MIL_REPO="$HOME/mil2/src"
+
+if [[ $(ps -p $$ | tail -n 1 | awk '{ print $4 }') == "zsh" ]]; then
+	source /opt/ros/jazzy/setup.zsh
+else
+	source /opt/ros/jazzy/setup.bash
+fi
+
+_list_complete() {
+	local THING
+        THINGS=("$1")
+	for THING in "${THINGS[@]}"; do
+		if [[ -z $2 || -n "$(echo "${THING:0:${#2}}" | grep "$2")" ]]; then
+			COMPREPLY+=("$THING")
+		fi
+	done
+}
+
+alias mil='cd $MIL_REPO'
+
+alias srcbrc="source ~/.bashrc"
+alias search_root='sudo find / ... | grep -i'
+alias search='find . -print | grep -i'
+alias fd="fdfind"
