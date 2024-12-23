f31d22c5cdb8f68ad7cd71e5f5e46860f6d581bf
DaniParr <daniel27parra@gmail.com>
2024-12-23 14:59:24 -0500

    Added files from mil to mil2 and built successfully

---
+++ src/subjugator/drivers/sub_actuator_board/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'sub_actuator_board'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mil2',
+    maintainer_email='mil2@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
