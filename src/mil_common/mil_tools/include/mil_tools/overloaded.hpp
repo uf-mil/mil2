00ebcc2125779ebab997884a7fb8b66a7e39952c
Cameron Brown <me@cbrxyz.com>
2025-01-17 00:39:02 -0500

    include/mil_tools/overloaded.hpp: Create variant overloaded helper class

---
+++ src/mil_common/mil_tools/include/mil_tools/overloaded.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace mil_tools::overloaded
+{
+template <class... Ts>
+struct make : Ts...
+{
+  using Ts::operator()...;
+};
+template <class... Ts>
+make(Ts...) -> make<Ts...>;
+}  // namespace mil_tools::overloaded
