2278330a3a7576c08b3c3826a1193bb940f8c467
Daniel Parra <daniel27parra@gmail.com>
2025-01-22 21:21:28 +0000

    Defined the render engine and removed sky for compatability with VMs

--- src/subjugator/simulation/subjugator_bringup/launch/gazebo.launch.py
+++ src/subjugator/simulation/subjugator_bringup/launch/gazebo.launch.py
@@ -23,13 +23,14 @@ def generate_launch_description():
 
     # Setup to launch the simulator and Gazebo world
     gz_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={'gz_args': PathJoinSubstitution([
-            pkg_project_gazebo,
-            'worlds',
-            'robosub_2024.world'
-        ])}.items(),
+        PythonLaunchDescriptionSource(os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={
+            'gz_args': [PathJoinSubstitution([
+                pkg_project_gazebo,
+                'worlds',
+                'robosub_2024.world'
+            ]), ' --render-engine', ' ogre']
+        }.items(),
     )
 
     # Spawn in Subjugator
