7cf1df30d783c04ed34d1bcec34259458b65e543
Alex Johnson <alexjohnson@ufl.edu>
2025-02-11 11:53:56 -0500

    setup IMU, DVL, and depth sensor params

--- src/subjugator/gnc/subjugator_localization/launch/subjugator_localization.launch.py
+++ src/subjugator/gnc/subjugator_localization/launch/subjugator_localization.launch.py
@@ -1,9 +1,82 @@
+from dataclasses import asdict, dataclass
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+AvailableTypes = str | list | float | int
+
+
+@dataclass
+class Parameter:
+    name: str
+    value: AvailableTypes
+
+    dict = asdict
+
 
 def generate_launch_description():
-    parameters = [{"name": "alex"}]
+    parameters: list[Parameter] = [
+        Parameter("imu0", "/imu/data_raw/imu"),
+        Parameter(
+            "imu0_config",
+            [
+                False,
+                False,
+                False,
+                True,
+                True,
+                True,
+                False,
+                False,
+                False,
+                True,
+                True,
+                False,
+                True,
+                True,
+                True,
+            ],
+        ),
+        Parameter("imu0_differential", True),
+        Parameter("imu0_relative", True),
+        Parameter("imu0_remove_gravitational_acceleration", True),
+        Parameter("imu0_queue_size", 10),
+        Parameter("odom0", "/dvl/odom"),
+        Parameter(
+            "odom0_config",
+            [
+                True,
+                True,
+                False,
+                False,
+                False,
+                True,
+                True,
+                False,
+                False,
+                False,
+                False,
+                True,
+                False,
+                False,
+                False,
+            ],
+        ),
+        Parameter("odom0_differential", True),
+        Parameter("odom0_relative", True),
+        Parameter("odom0_queue_size", 10),
+        #        Parameter("pose0", "/depth/pose"),
+        #        Parameter("pose0_config", [False, False, True,
+        #                                   False, False, False,
+        #                                   False, False, False,
+        #                                   False, False, False,
+        #                                   False, False, False]),
+        #        Parameter("pose0_differential", True),
+        #        Parameter("pose0_relative", True),
+        #        Parameter("pose0_queue_size", 10),
+        Parameter("print_diagnostics", True),
+        Parameter("publish_acceleration", True),
+    ]
     return LaunchDescription(
         [
             Node(
@@ -11,7 +84,7 @@ def generate_launch_description():
                 namespace="subjugator_localization",
                 executable="ekf_localization_node",
                 name="ekf_node",
-                parameters=parameters,
+                parameters=[asdict(p) for p in parameters],
             ),
         ],
     )
