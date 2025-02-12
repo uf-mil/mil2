9ac19ebebf7ddedb91ac704968e324a7fb5a4889
Praveen Perera <withanage.perera@athena-tek.com>
2025-02-12 15:50:46 -0500

    Created a test script to see the outputs of libsensors.

---
+++ src/mil_common/mil_tools/hardware/test.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <sensors/sensors.h>
+
+using std::cout;
+
+int main() 
+{
+    // Initialize sensors, none-zero returned value means there was an error during initialization
+    if (sensors_init(nullptr) != 0) 
+    {
+        std::cerr << "Failed to initialize sensors" << std::endl;
+        return 1;
+    }
+
+    // Iterate through all chips
+    const sensors_chip_name *chip;
+    
+    // Specify starting chip as 0 so that we go through all available chips
+    int chip_nr = 0;
+    
+    // Pass a nullptr for the sensors_chip_name argument to tell the function to read all available chips
+    // since we might change our CPU or components.
+    while ((chip = sensors_get_detected_chips(nullptr, &chip_nr)) != nullptr) 
+    {
+        // Get all features (sensors) of the chip
+        const sensors_feature *feature;
+        int feature_nr = 0;
+        
+        while ((feature = sensors_get_features(chip, &feature_nr))) 
+        {
+            // We only care about temperature readings so any feature that's not temp we can skip
+            if (feature->type != SENSORS_FEATURE_TEMP) 
+            {
+                continue;
+            }
+
+            // Get temperature reading
+            double temp;
+            const sensors_subfeature *subfeature = sensors_get_subfeature(chip, feature, SENSORS_SUBFEATURE_TEMP_INPUT);
+                
+            // Make sure we didn't get a nullptr
+            if (subfeature) 
+            {
+                if (sensors_get_value(chip, subfeature->number, &temp) == 0) 
+                {
+                    // Get sensor label
+                    char *label = sensors_get_label(chip, feature);
+
+                    cout << label << ": " << temp << "\n";
+
+                    // Free the string
+                    free(label);
+                }
+            }
+        }
+    }
+
+    sensors_cleanup();
+    return 0;
+}
