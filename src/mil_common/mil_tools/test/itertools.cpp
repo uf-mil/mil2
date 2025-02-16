d3f416fb6dbf46c8881202624de9019b3045327a
jojongx <jbenigno@ufl.edu>
2025-02-16 15:22:26 -0500

    Made tests and editing CMakeLists

--- src/mil_common/mil_tools/test/itertools.cpp
+++ src/mil_common/mil_tools/test/itertools.cpp
@@ -6,9 +6,23 @@
 
 TEST(mil_tools_itertools, enumerate)
 {
-  std::vector<std::pair<int, int>> nums = {{0, 10}, {1, 20}, {2, 30}};
-  std::vector<std::pair<int, std::string>> strings = {{0, "first"}, {1, "second"}, {2, "third"}};
-  EXPECT_EQ(mil_tools::itertools::enumerate({10, 20, 30}), nums);
+  std::vector<std::pair<int, int>> exp_nums = {{0, 10}, {1, 20}, {2, 30}};
+  std::vector<int> sample_nums = {10, 20, 30};
+  std::vector<std::pair<int, int>> test_nums;
+  for (const auto& [index, value] : mil_tools::itertools::enumerate(sample_nums))
+  {
+    test_nums.push_back({index, value});
+  }
+  EXPECT_EQ(test_nums, exp_nums);
+
+  std::vector<std::pair<int, std::string>> exp_strings = {{0, "first"}, {1, "second"}, {2, "third"}};
+  std::vector<std::string> sample_strings = {"first", "second", "third"};
+  std::vector<std::pair<int, std::string>> test_strings;
+  for (const auto& [index, value] : mil_tools::itertools::enumerate(sample_strings))
+  {
+    test_strings.push_back({index, value});
+  }
+  EXPECT_EQ(test_strings, exp_strings);
 }
 
 int main(int argc, char **argv)
