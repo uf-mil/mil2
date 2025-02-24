f41996c3d1ccf0c21ca0a8eebf90aab25f8ae6b5
jojongx <jbenigno@ufl.edu>
2025-02-24 14:51:09 -0500

    Fixed build problems

--- src/mil_common/mil_tools/src/itertools.cpp
+++ src/mil_common/mil_tools/src/itertools.cpp
@@ -2,26 +2,5 @@
 
 namespace mil_tools::itertools
 {
-// https://www.reedbeta.com/blog/python-like-enumerate-in-cpp17/
-template <typename T,
-          typename TIter,
-          typename>
-constexpr auto enumerate(T && iterable)
-{
-    struct iterator
-    {
-        size_t i;
-        TIter iter;
-        bool operator != (const iterator & other) const { return iter != other.iter; }
-        void operator ++ () { ++i; ++iter; }
-        auto operator * () const { return std::tie(i, *iter); }
-    };
-    struct iterable_wrapper
-    {
-        T iterable;
-        auto begin() { return iterator{ 0, std::begin(iterable) }; }
-        auto end() { return iterator{ 0, std::end(iterable) }; }
-    };
-    return iterable_wrapper{ std::forward<T>(iterable) };
-}
+
 }  // namespace mil_tools::itertools
