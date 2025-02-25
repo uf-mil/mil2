36c3129c0d3f044fea03af26de4b790b3fbdd367
Alex Johnson <alexjohnson@ufl.edu>
2025-02-25 11:39:24 -0500

    thruster_manager: now uses parameters and new custom msg output type

---
+++ src/subjugator/gnc/subjugator_thruster_manager/src/thruster_manager.cpp
@@ -0,0 +1,95 @@
+#include <Eigen/Dense>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "geometry_msgs/msg/wrench.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "subjugator_msgs/msg/thruster_efforts.hpp"
+
+class ThrusterManager : public rclcpp::Node
+{
+public:
+  ThrusterManager();
+
+private:
+  rclcpp::Subscription<geometry_msgs::msg::Wrench>::SharedPtr wrench_subscription_;
+  Eigen::VectorXd reference_wrench_;  // TODO: change to Vector6d
+  Eigen::MatrixXd tam_;
+  int const dof_ = 6;
+  int const thruster_count_ = 8;
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<subjugator_msgs::msg::ThrusterEfforts>::SharedPtr thrust_publisher_;
+
+  void wrench_callback(geometry_msgs::msg::Wrench::SharedPtr msg);
+  void timer_callback();
+};
+
+// Construct node class
+ThrusterManager::ThrusterManager() : Node("thruster_manager")
+{
+  reference_wrench_ = Eigen::VectorXd::Zero(6);
+
+  // Create thruster allocation matrix from config file parameters
+  tam_ = Eigen::MatrixXd::Zero(dof_, thruster_count_);
+  std::vector<std::string> axes = { "x", "y", "z", "rx", "ry", "rz" };
+  for (unsigned int i = 0; i < axes.size(); i++)
+  {
+    std::string param_name = "tam." + axes[i];
+    this->declare_parameter(param_name, std::vector<double>(thruster_count_, 0.0));
+    tam_.row(i) =
+        Eigen::Map<Eigen::RowVectorXd const>(this->get_parameter(param_name).as_double_array().data(), thruster_count_);
+  }
+  // Throw error if TAM is all zeroes (ran without params instead of launched)
+  if (tam_.isZero())
+  {
+    RCLCPP_ERROR(this->get_logger(), "Thruster Allocation Matrix is all zero. Ensure you are launching the node with "
+                                     "ros2 launch, and that config values are set.");
+  }
+
+  wrench_subscription_ = this->create_subscription<geometry_msgs::msg::Wrench>(
+      "wrench", 1, [this](geometry_msgs::msg::Wrench::SharedPtr const msg) -> void { this->wrench_callback(msg); });
+
+  thrust_publisher_ = this->create_publisher<subjugator_msgs::msg::ThrusterEfforts>("thruster_efforts", 1);
+  RCLCPP_INFO(this->get_logger(), "Publishing to: %s", thrust_publisher_->get_topic_name());
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), [this]() -> void { this->timer_callback(); });
+}
+
+// Update wrench when new msg heard
+void ThrusterManager::wrench_callback(geometry_msgs::msg::Wrench::SharedPtr msg)
+{
+  RCLCPP_INFO(this->get_logger(), "Heard wrench: [%.2f, %.2f, %.2f], [%.2f, %.2f, %.2f]", msg->force.x, msg->force.y,
+              msg->force.z, msg->torque.x, msg->torque.y, msg->torque.z);
+
+  this->reference_wrench_ << msg->force.x, msg->force.y, msg->force.z, msg->torque.x, msg->torque.y, msg->torque.z;
+}
+
+// Compute and publish thruster efforts
+void ThrusterManager::timer_callback()
+{
+  Eigen::VectorXd const thrust_values(tam_.completeOrthogonalDecomposition().pseudoInverse() * reference_wrench_);
+
+  auto msg = subjugator_msgs::msg::ThrusterEfforts();
+  msg.thrust_frh = thrust_values[0];
+  msg.thrust_flh = thrust_values[1];
+  msg.thrust_brh = thrust_values[2];
+  msg.thrust_blh = thrust_values[3];
+  msg.thrust_frv = thrust_values[4];
+  msg.thrust_flv = thrust_values[5];
+  msg.thrust_brv = thrust_values[6];
+  msg.thrust_blv = thrust_values[7];
+
+  this->thrust_publisher_->publish(msg);
+}
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ThrusterManager>());
+  rclcpp::shutdown();
+
+  return 0;
+}
