edd3cd7327c42f8e43d82c8c15445ec51e4ace69
Ricky Zhang <rickyzhang196@outlook.com>
2025-03-02 02:18:36 -0500

    Remove extraneous cmake folder

--- src/subjugator/command/subjugator_keyboard_control/cmake-build-debug-wsl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ricky/mil2/src/subjugator/drivers/subjugator_keyboard_control
-# Build directory: /home/ricky/mil2/src/subjugator/drivers/subjugator_keyboard_control/cmake-build-debug-wsl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
