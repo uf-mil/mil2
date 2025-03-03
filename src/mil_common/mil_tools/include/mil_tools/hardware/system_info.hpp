31946d33aea343315bf21fb40cf699fb61fdd747
Praveen Perera <withanage.perera@athena-tek.com>
2025-03-02 23:45:37 -0500

    Forgot to add a namespace for the system info.

--- src/mil_common/mil_tools/include/mil_tools/hardware/system_info.hpp
+++ src/mil_common/mil_tools/include/mil_tools/hardware/system_info.hpp
@@ -2,4 +2,12 @@
 
 #include <string>
 
-std::string get_cpu_architecture();
+namespace mil_tools::hardware::system_info
+{
+    /**
+     * @brief Uses "uname -m" to get the CPU architecture.
+     * 
+     * @return std::string 
+     */
+    std::string get_cpu_architecture();
+}
