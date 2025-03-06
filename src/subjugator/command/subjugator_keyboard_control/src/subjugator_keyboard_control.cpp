15405d6bb3aa6c053091979c49e25837ba0a64a8
Alex Johnson <alexjohnson@ufl.edu>
2025-03-06 15:02:15 -0500

    Modified key mappings

--- src/subjugator/command/subjugator_keyboard_control/src/subjugator_keyboard_control.cpp
+++ src/subjugator/command/subjugator_keyboard_control/src/subjugator_keyboard_control.cpp
@@ -1,42 +1,51 @@
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include <atomic>
 #include <chrono>
 #include <iostream>
 #include <thread>
-#include <atomic>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include "rclcpp/rclcpp.hpp"
+
 #include "geometry_msgs/msg/wrench.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
 using namespace std::chrono;
-using std::thread;
-using std::cout;
 using std::atomic;
+using std::cout;
+using std::thread;
 
-constexpr int PUBLISH_RATE = 10; // Hertz
+constexpr int PUBLISH_RATE = 10;  // Hertz
 
 // May be used elsewhere if similar logic?
-struct KeyState {
+struct KeyState
+{
   bool pressed = false;
   steady_clock::time_point last_time = steady_clock::now();
 };
-struct AxisEffect {
+struct AxisEffect
+{
   KeyState* key;
   double multiplier;
-  double* target; // What we're updating
+  double* target;  // What we're updating
 };
 
-class SubjugatorKeyboardControl final: public rclcpp::Node {
+class SubjugatorKeyboardControl final : public rclcpp::Node
+{
 public:
   SubjugatorKeyboardControl()
-    : Node("subjugator_keyboard_control"),
-      force_x_(0.0), force_y_(0.0), force_z_(0.0),
-      torque_x_(0.0), torque_y_(0.0), torque_z_(0.0),
-      running_(true)
+    : Node("subjugator_keyboard_control")
+    , force_x_(0.0)
+    , force_y_(0.0)
+    , force_z_(0.0)
+    , torque_x_(0.0)
+    , torque_y_(0.0)
+    , torque_z_(0.0)
+    , running_(true)
   {
     publisher_ = this->create_publisher<geometry_msgs::msg::Wrench>("cmd_wrench", PUBLISH_RATE);
-    this->declare_parameter("linear_speed", 0.5);
+    this->declare_parameter("linear_speed", 1.0);
     this->declare_parameter("angular_speed", 1.0);
     base_linear_ = this->get_parameter("linear_speed").as_double();
     base_angular_ = this->get_parameter("angular_speed").as_double();
@@ -44,31 +53,33 @@ public:
     initTerminal();
 
     // Start keyboard and publisher threads
-    keyboard_thread_  = thread(&SubjugatorKeyboardControl::keyboardLoop, this);
+    keyboard_thread_ = thread(&SubjugatorKeyboardControl::keyboardLoop, this);
     publisher_thread_ = thread(&SubjugatorKeyboardControl::publishLoop, this);
 
-        cout << R"(Subjugator Keyboard Control:
-      Up          : +x force
-      Down        : -x force
-      Right       : +y force
-      Left        : -y force
-      Shift + Up  : +z force
-      Shift + Down: -z force
-      w           : +roll (torque x)
-      s           : -roll (torque x)
-      a           : -yaw (torque z)
-      d           : +yaw (torque z)
-      Shift + w   : +pitch (torque y)
-      Shift + s   : -pitch (torque y)
-      Space       : Stop all motion
+    cout << R"(Subjugator Keyboard Control:
+      w           : +x force
+      s           : -x force
+      a           : +y force
+      d           : -y force
+      x           : +z force
+      z           : -z force
+      Up          : pitch up   (-torque y)
+      Down        : pitch down (+torque y)
+      Right       : yaw right  (-torque z)
+      Left        : yaw left   (+torque z)
+      e           : roll left  (-torque x)
+      r           : roll right (+torque x)
       q           : Quit
     )" << '\n';
   }
 
-  ~SubjugatorKeyboardControl() override {
+  ~SubjugatorKeyboardControl() override
+  {
     running_ = false;
-    if (keyboard_thread_.joinable()) keyboard_thread_.join();
-    if (publisher_thread_.joinable()) publisher_thread_.join();
+    if (keyboard_thread_.joinable())
+      keyboard_thread_.join();
+    if (publisher_thread_.joinable())
+      publisher_thread_.join();
     restoreTerminal();
   }
 
@@ -82,14 +93,15 @@ private:
   atomic<bool> running_;
 
   termios old_terminal_settings_{};
-  bool terminal_initialized_{false};
+  bool terminal_initialized_{ false };
 
-  void initTerminal() {
+  void initTerminal()
+  {
     tcgetattr(STDIN_FILENO, &old_terminal_settings_);
     termios new_settings = old_terminal_settings_;
     new_settings.c_lflag &= ~(ICANON | ECHO);
-    new_settings.c_cc[VMIN] = 0;  // Set non-blocking input
-    new_settings.c_cc[VTIME] = 1; // 0.1 second timeout
+    new_settings.c_cc[VMIN] = 0;   // Set non-blocking input
+    new_settings.c_cc[VTIME] = 1;  // 0.1 second timeout
     // These flags are necessary so doesn't always return -1
     int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
@@ -97,48 +109,45 @@ private:
     terminal_initialized_ = true;
   }
 
-  void restoreTerminal() const {
-    if (terminal_initialized_) {
+  void restoreTerminal() const
+  {
+    if (terminal_initialized_)
+    {
       tcsetattr(STDIN_FILENO, TCSANOW, &old_terminal_settings_);
     }
   }
 
-  void keyboardLoop() {
+  void keyboardLoop()
+  {
     KeyState arrow_up, arrow_down, arrow_left, arrow_right;
-    KeyState arrow_up_shift, arrow_down_shift;
-    KeyState key_w, key_s, key_a, key_d, key_shift_w, key_shift_s;
+    KeyState key_e, key_r;
+    KeyState key_w, key_s, key_a, key_d, key_z, key_x;
 
     double current_force_x = 0.0, current_force_y = 0.0, current_force_z = 0.0;
     double current_torque_x = 0.0, current_torque_y = 0.0, current_torque_z = 0.0;
 
-    std::vector<KeyState*> keys = {
-      &arrow_up, &arrow_down, &arrow_left, &arrow_right,
-      &arrow_up_shift, &arrow_down_shift,
-      &key_w, &key_s, &key_a, &key_d, &key_shift_w, &key_shift_s
-    };
+    std::vector<KeyState*> keys = { &arrow_up, &arrow_down, &arrow_left, &arrow_right, &key_e, &key_r,
+                                    &key_w,    &key_s,      &key_a,      &key_d,       &key_z, &key_x };
 
     AxisEffect letter_effects[] = {
-      { &arrow_up,         base_linear_,  &current_force_x },
-      { &arrow_down,      -base_linear_,  &current_force_x },
-      { &arrow_right,      base_linear_,  &current_force_y },
-      { &arrow_left,      -base_linear_,  &current_force_y },
-      { &arrow_up_shift,   base_linear_,  &current_force_z },
-      { &arrow_down_shift, -base_linear_,  &current_force_z },
-      { &key_w,    base_angular_, &current_torque_x },
-      { &key_s,   -base_angular_, &current_torque_x },
-      { &key_a,   -base_angular_, &current_torque_z },
-      { &key_d,    base_angular_, &current_torque_z },
-      { &key_shift_w,    base_angular_, &current_torque_y },
-      { &key_shift_s,   -base_angular_, &current_torque_y }
+      { &arrow_up, -base_angular_, &current_torque_y },    { &arrow_down, base_angular_, &current_torque_y },
+      { &arrow_right, -base_angular_, &current_torque_z }, { &arrow_left, base_angular_, &current_torque_z },
+      { &key_e, -base_angular_, &current_torque_x },       { &key_r, base_angular_, &current_torque_x },
+      { &key_w, base_linear_, &current_force_x },          { &key_s, -base_linear_, &current_force_x },
+      { &key_a, base_linear_, &current_force_y },          { &key_d, -base_linear_, &current_force_y },
+      { &key_x, base_linear_, &current_force_z },          { &key_z, -base_linear_, &current_force_z },
     };
-    const auto timeout = milliseconds(150);
+    auto const timeout = milliseconds(150);
 
-    while (running_) {
+    while (running_)
+    {
       auto now = steady_clock::now();
 
       // Update key timeouts first?
-      for (auto key : keys) {
-        if (now - key->last_time > timeout) key->pressed = false;
+      for (auto key : keys)
+      {
+        if (now - key->last_time > timeout)
+          key->pressed = false;
       }
 
       // Reset every time
@@ -146,38 +155,77 @@ private:
       current_torque_x = 0.0, current_torque_y = 0.0, current_torque_z = 0.0;
 
       int ch = getchar();
-      if (ch == -1) {
+      if (ch == -1)
+      {
         // Do nothing. Avoids additional nesting or use of goto
-      } else if (ch == 27) {  // Possible arrow key escape sequence
-        if (getchar() == 91) {
+      }
+      else if (ch == 27)
+      {  // Possible arrow key escape sequence
+        if (getchar() == 91)
+        {
           int ch3 = getchar();
-          if (ch3 == '1') { // Possibly a shifted arrow key
-            if (getchar() == ';' && getchar() == '2') {
-                int ch6 = getchar();
-                if (ch6 == 'A') { arrow_up_shift.pressed = true; arrow_up_shift.last_time = now; }
-                else if (ch6 == 'B') { arrow_down_shift.pressed = true; arrow_down_shift.last_time = now; }
-                else { cout << "Unknown shift arrow sequence\n"; }
-            }
-          } else if (ch3 == 'A') { arrow_up.pressed = true; arrow_up.last_time = now; }
-          else if (ch3 == 'B') { arrow_down.pressed = true; arrow_down.last_time = now; }
-          else if (ch3 == 'C') { arrow_right.pressed = true; arrow_right.last_time = now; }
-          else if (ch3 == 'D') { arrow_left.pressed = true; arrow_left.last_time = now; }
-          else { cout << "Unknown escape sequence\n"; }
+          if (ch3 == 'A')
+          {
+            arrow_up.pressed = true;
+            arrow_up.last_time = now;
+          }
+          else if (ch3 == 'B')
+          {
+            arrow_down.pressed = true;
+            arrow_down.last_time = now;
+          }
+          else if (ch3 == 'C')
+          {
+            arrow_right.pressed = true;
+            arrow_right.last_time = now;
+          }
+          else if (ch3 == 'D')
+          {
+            arrow_left.pressed = true;
+            arrow_left.last_time = now;
+          }
+          else
+          {
+            cout << "Unknown escape sequence\n";
+          }
         }
-      } else {
+      }
+      else
+      {
         // Process normal keys:
-        switch(ch) {
-          case 'w': key_w.pressed = true; key_w.last_time = now; break;
-          case 's': key_s.pressed = true; key_s.last_time = now; break;
-          case 'a': key_a.pressed = true; key_a.last_time = now; break;
-          case 'd': key_d.pressed = true; key_d.last_time = now; break;
-          case 'W': key_shift_w.pressed = true; key_shift_w.last_time = now; break;
-          case 'S': key_shift_s.pressed = true; key_shift_s.last_time = now; break;
-          case ' ':
-            arrow_up.pressed = arrow_down.pressed = arrow_left.pressed = arrow_right.pressed = false;
-            arrow_up_shift.pressed = arrow_down_shift.pressed = false;
-            key_w.pressed = key_s.pressed = key_a.pressed = key_d.pressed = false;
-            key_shift_w.pressed = key_shift_s.pressed = false;
+        switch (ch)
+        {
+          case 'w':
+            key_w.pressed = true;
+            key_w.last_time = now;
+            break;
+          case 's':
+            key_s.pressed = true;
+            key_s.last_time = now;
+            break;
+          case 'a':
+            key_a.pressed = true;
+            key_a.last_time = now;
+            break;
+          case 'd':
+            key_d.pressed = true;
+            key_d.last_time = now;
+            break;
+          case 'e':
+            key_e.pressed = true;
+            key_e.last_time = now;
+            break;
+          case 'r':
+            key_r.pressed = true;
+            key_r.last_time = now;
+            break;
+          case 'z':
+            key_z.pressed = true;
+            key_z.last_time = now;
+            break;
+          case 'x':
+            key_x.pressed = true;
+            key_x.last_time = now;
             break;
           case 'q':
             running_ = false;
@@ -189,8 +237,10 @@ private:
         }
       }
 
-      for (auto& effect : letter_effects) {
-        if (effect.key->pressed) {
+      for (auto& effect : letter_effects)
+      {
+        if (effect.key->pressed)
+        {
           *(effect.target) += effect.multiplier;
         }
       }
@@ -207,14 +257,15 @@ private:
     }
   }
 
-
-  void publishLoop() const {
+  void publishLoop() const
+  {
     rclcpp::Rate rate(PUBLISH_RATE);
-    while (rclcpp::ok() && running_) {
+    while (rclcpp::ok() && running_)
+    {
       auto current_msg = geometry_msgs::msg::Wrench();
-      current_msg.force.x  = force_x_.load();
-      current_msg.force.y  = force_y_.load();
-      current_msg.force.z  = force_z_.load();
+      current_msg.force.x = force_x_.load();
+      current_msg.force.y = force_y_.load();
+      current_msg.force.z = force_z_.load();
       current_msg.torque.x = torque_x_.load();
       current_msg.torque.y = torque_y_.load();
       current_msg.torque.z = torque_z_.load();
@@ -225,13 +276,13 @@ private:
   }
 };
 
-int main(const int argc, char** argv)
+int main(int const argc, char** argv)
 {
-//  while (true) {
-//    int ch = getchar();
-//    std::cout << "Key code: " << ch << std::endl;
-//  }
-//  return 0;
+  //  while (true) {
+  //    int ch = getchar();
+  //    std::cout << "Key code: " << ch << std::endl;
+  //  }
+  //  return 0;
   rclcpp::init(argc, argv);
   auto node = std::make_shared<SubjugatorKeyboardControl>();
   rclcpp::spin(node);
