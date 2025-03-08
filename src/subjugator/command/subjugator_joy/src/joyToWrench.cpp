92d624e7f90fb6c3a0eadf24ca431a3f038df1d5
Alex Johnson <alexjohnson@ufl.edu>
2025-03-08 10:44:15 -0500

    modified joystick bindings

--- src/subjugator/command/subjugator_joy/src/joyToWrench.cpp
+++ src/subjugator/command/subjugator_joy/src/joyToWrench.cpp
@@ -6,13 +6,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
 
-using namespace std::chrono_literals;
 using namespace std;
-// ls /opt/ros/jazzy/include/sensor_msgs/sensor_msgs/msg/
-
-/* This example creates a subclass of Node and uses a fancy C++11 lambda
- * function to shorten the callback syntax, at the expense of making the
- * code somewhat more difficult to understand at first glance. */
 
 class JoySubscriber : public rclcpp::Node
 {
@@ -27,25 +21,18 @@ class JoySubscriber : public rclcpp::Node
             // the right button for directional pad gives negative values
             // x and z for force values are also flipped (negative positive)
 
-            wrench_msg.force.x = msg->axes[0];  // move right and left with left stick horizontal
-            wrench_msg.force.y = msg->axes[1];  // move back and forth with left stick vertical
-            wrench_msg.force.z = msg->axes[4];  // move up and down with right stick vertical
+            wrench_msg.force.x = msg->axes[1];  // move back and forth with left stick vertical
+            wrench_msg.force.y = msg->axes[0];  // move right and left with left stick horizontal
+            wrench_msg.force.z =
+                (msg->axes[7] == 1) ? 1.0 : ((msg->axes[7] == -1.0) ? -1.0 : 0.0);  // move up and down with up and down
+                                                                                    // directional pad for roll
 
-            // buttons for torque, rotating around axis
-            // button for positive torque and another for negative for each of them
-            // using directional pad for roll (probably least used)
+            wrench_msg.torque.x = (msg->axes[6] == 1) ?
+                                      1.0 :
+                                      ((msg->axes[6] == -1.0) ? -1.0 : 0.0);  // left and right directional pad for roll
+            wrench_msg.torque.y = msg->axes[4];                               // right stick vertical for pitch
+            wrench_msg.torque.z = msg->axes[3];                               // right stick horizontal for pitch
 
-            wrench_msg.torque.x =
-                (msg->axes[6] == 1) ? 1.0 : ((msg->axes[6] == -1.0) ? -1.0 : 0.0);         // x and b button for roll
-            wrench_msg.torque.y = msg->buttons[3] ? 1.0 : (msg->buttons[0] ? -1.0 : 0.0);  // y and a buttons for pitch
-                                                                                           // (pointing down or up)
-            wrench_msg.torque.z =
-                msg->buttons[1] ? 1.0 :
-                                  (msg->buttons[2] ? -1.0 : 0.0);  // left and right of directional pad button for yaw
-
-            // RCLCPP_INFO(this->get_logger(), "Force: [%.2f, %.2f, %.2f], Torque: [%.1f, %.1f, %.1f]",
-            // wrench_msg.force.x, wrench_msg.force.y, wrench_msg.force.z, wrench_msg.torque.x, wrench_msg.torque.y,
-            // wrench_msg.torque.z);
             this->publisher_1->publish(wrench_msg);
         };
         subscription_1 = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, joy_callback);
