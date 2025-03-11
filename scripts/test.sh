9a415dc08133aa0b4dd628a1bd2c7e2fc0d3c0af
WithanagePerera <prav.perera@gmail.com>
2025-03-11 14:23:16 -0400

    Wrote a unit test for the system_info script. Couldn't write a unit test for the cpu_temp script since the values are dynamic, and it would be difficult to use an injected value into a file descriptor since tegrastats isn't available on the CI's VM.

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-echo "Dir: $SCRIPT_DIR"
-echo "Compare: $HOME/mil2/scripts"
-
-if [[ "$SCRIPT_DIR" != "$HOME/mil2/scripts" && -z ${ALLOW_NONSTANDARD_DIR:-}  ]]; then
-  echo "The directories are different."
-else
-  echo "The directories are the same."
-fi
---
+++ src/mil_common/mil_tools/test/hardware/system_info.cpp
@@ -0,0 +1,37 @@
+#include "mil_tools/hardware/system_info.hpp"
+
+#include <gtest/gtest.h>
+#include <cstdlib>
+#include <string>
+
+TEST(mil_tools_hardware_system_info, get_cpu_architecture)
+{
+  // Create a pipe so I can execute "arch" and extract the architecture
+  FILE* pipe = popen("arch", "r");
+  
+  // Buffer to contain the output
+  char buffer[10];
+  std::string operandA = "";
+  
+  // Continues until we reach the end of the pipe's output
+  while (!feof(pipe)) 
+  {
+      if (fgets(buffer, 10, pipe) != nullptr) 
+      {
+          operandA += buffer;
+      }
+  }
+  
+  pclose(pipe);
+  
+  // Remove the newline (if any)
+  if (!operandA.empty() && operandA[operandA.length()-1] == '\n') 
+  {
+      operandA.erase(operandA.length()-1);
+  }
+
+  // Store the result of the get_cpu_architecture function for our comparison
+  std::string operandB = mil_tools::hardware::system_info::get_cpu_architecture();
+
+  ASSERT_EQ(operandA.compare(operandB), 0);
+}
