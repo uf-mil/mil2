d5d1c110b37ea21f9ff8d316824b36221860e8b5
Mohana <mohanashree.pamidimukkala@gmail.com>
2025-03-14 12:08:11 -0400

    action spaces for sub_env

--- src/subjugator/subjugator_RL/sub_env.py
+++ src/subjugator/subjugator_RL/sub_env.py
@@ -8,4 +8,9 @@ import time
 class subEnv(gym.Env):
 	def _init_(self, render_mode = "rgb_array"):
 		self.observation_space = spaces.Box(0, 255, shape(200, 266), dtype=np.uint8)
-		
+		self.action_space = spaces.Discrete(6)
+
+		self.sketch = None
+		self.setup = True
+
+		self._action_to_direction= {
