8a5453a4eb3a5396cdb76f4682ef583e3cb3c177
Ricky Zhang <rickyzhang196@outlook.com>
2025-03-15 00:07:18 -0400

    fix linting

--- src/subjugator/gnc/subjugator_localization/launch/subjugator_localization.launch.py
+++ src/subjugator/gnc/subjugator_localization/launch/subjugator_localization.launch.py
@@ -1,21 +1,25 @@
 import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
     config_file = os.path.join(
-        get_package_share_directory('subjugator_localization'),
-        'config',
-        'parameter_definitions.yaml'
+        get_package_share_directory("subjugator_localization"),
+        "config",
+        "parameter_definitions.yaml",
     )
 
-    return LaunchDescription([
-        Node(
-            package='robot_localization',
-            executable='ukf_node',
-            name='ukf_filter_node',
-            namespace='subjugator_localization',
-            parameters=[config_file],
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="robot_localization",
+                executable="ukf_node",
+                name="ukf_filter_node",
+                namespace="subjugator_localization",
+                parameters=[config_file],
+            ),
+        ],
+    )
