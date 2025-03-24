81e378b422f2676a84a0a9d39e84453d4ec2dd0c
Carlos Chavez <cforcarlos22@gmail.com>
2025-03-24 10:49:23 -0400

    applied pre-commit

--- src/subjugator/simulation/subjugator_gazebo/include/ScoringPlugin.hh
+++ src/subjugator/simulation/subjugator_gazebo/include/ScoringPlugin.hh
@@ -1,54 +1,46 @@
 #ifndef SCORING_PLUGIN_HH
 #define SCORING_PLUGIN_HH
 
-#include <gz/sim/System.hh>
+#include <gz/plugin/Register.hh>
 #include <gz/sim/Entity.hh>
 #include <gz/sim/EntityComponentManager.hh>
+#include <gz/sim/System.hh>
 #include <gz/sim/components/Pose.hh>
-#include <gz/plugin/Register.hh>
-
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/int32.hpp>
 
 namespace scoring_plugin
 {
-  class ScoringPlugin : public gz::sim::System,
-                        public gz::sim::ISystemConfigure,
-                        public gz::sim::ISystemPostUpdate
-  {
+class ScoringPlugin : public gz::sim::System, public gz::sim::ISystemConfigure, public gz::sim::ISystemPostUpdate
+{
   public:
-    void Configure(const gz::sim::Entity &entity,
-                   const std::shared_ptr<const sdf::Element> &sdf,
-                   gz::sim::EntityComponentManager &ecm,
-                   gz::sim::EventManager &eventMgr) override;
+    void Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
+                   gz::sim::EntityComponentManager &ecm, gz::sim::EventManager &eventMgr) override;
 
-    void PostUpdate(const gz::sim::UpdateInfo &info,
-                    const gz::sim::EntityComponentManager &ecm) override;
+    void PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityComponentManager const &ecm) override;
 
   private:
-    bool SubPassedThroughGate(const gz::math::Pose3d &subPose,
-                              const gz::math::Pose3d &gatePose);
-    double getAngleBetweenVectors(const gz::math::Pose3d &subPose, 
-                              const gz::math::Pose3d &gatePose);
+    bool SubPassedThroughGate(gz::math::Pose3d const &subPose, gz::math::Pose3d const &gatePose);
+    double getAngleBetweenVectors(gz::math::Pose3d const &subPose, gz::math::Pose3d const &gatePose);
 
   private:
     // The submarine’s entity (this plugin’s parent model)
-    gz::sim::Entity subEntity_{gz::sim::kNullEntity};
+    gz::sim::Entity subEntity_{ gz::sim::kNullEntity };
 
     // The gate’s entity, found by name
-    gz::sim::Entity gateEntity_{gz::sim::kNullEntity};
+    gz::sim::Entity gateEntity_{ gz::sim::kNullEntity };
 
     // The gate’s model name (passed in via SDF <gate_name>)
     std::string gateModelName_;
 
     // Score tracking
-    int score_{0};
-    bool gatePassed_{false};
+    int score_{ 0 };
+    bool gatePassed_{ false };
 
     // ROS
     rclcpp::Node::SharedPtr rosNode_;
     rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr scorePub_;
-  };
-}
+};
+}  // namespace scoring_plugin
 
 #endif
--- src/subjugator/simulation/subjugator_gazebo/src/ScoringPlugin.cc
+++ src/subjugator/simulation/subjugator_gazebo/src/ScoringPlugin.cc
@@ -1,156 +1,143 @@
 #include "ScoringPlugin.hh"
+
+#include <cmath>
+#include <gz/common/Console.hh>
+#include <gz/math/Helpers.hh>
+#include <gz/plugin/Register.hh>
 #include <gz/sim/Model.hh>
 #include <gz/sim/components/Name.hh>
-#include <gz/common/Console.hh>
-#include <gz/plugin/Register.hh>
-#include <gz/math/Helpers.hh> 
-#include <cmath>
 #include <iostream>
 
-GZ_ADD_PLUGIN(scoring_plugin::ScoringPlugin,
-              gz::sim::System,
-              scoring_plugin::ScoringPlugin::ISystemConfigure,
+GZ_ADD_PLUGIN(scoring_plugin::ScoringPlugin, gz::sim::System, scoring_plugin::ScoringPlugin::ISystemConfigure,
               scoring_plugin::ScoringPlugin::ISystemPostUpdate)
 
 namespace scoring_plugin
 {
 
-
-void ScoringPlugin::Configure(const gz::sim::Entity &entity,
-                              const std::shared_ptr<const sdf::Element> &sdf,
-                              gz::sim::EntityComponentManager &ecm,
-                              gz::sim::EventManager & /*eventMgr*/)
+void ScoringPlugin::Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
+                              gz::sim::EntityComponentManager &ecm, gz::sim::EventManager & /*eventMgr*/)
 {
-  this->subEntity_ = entity;
+    this->subEntity_ = entity;
 
-  // get gate model name
-  if (sdf->HasElement("gate_name"))
-    this->gateModelName_ = sdf->Get<std::string>("gate_name");
-  else
-    gzerr << "[ScoringPlugin] Missing <gate_name> parameter in SDF!\n";
+    // get gate model name
+    if (sdf->HasElement("gate_name"))
+        this->gateModelName_ = sdf->Get<std::string>("gate_name");
+    else
+        gzerr << "[ScoringPlugin] Missing <gate_name> parameter in SDF!\n";
 
-  ecm.Each<gz::sim::components::Name>(
-    [&](const gz::sim::Entity &_entity, const gz::sim::components::Name *_nameComp)
-    {
-      if (_nameComp->Data() == this->gateModelName_)
-      {
-        this->gateEntity_ = _entity;
-        gzdbg << "[ScoringPlugin] Found gate entity: " << _entity << std::endl;
-      }
-      return true;
-    }
-  );
+    ecm.Each<gz::sim::components::Name>(
+        [&](gz::sim::Entity const &_entity, gz::sim::components::Name const *_nameComp)
+        {
+            if (_nameComp->Data() == this->gateModelName_)
+            {
+                this->gateEntity_ = _entity;
+                gzdbg << "[ScoringPlugin] Found gate entity: " << _entity << std::endl;
+            }
+            return true;
+        });
 
-  if (!rclcpp::ok())
-    rclcpp::init(0, nullptr);
+    if (!rclcpp::ok())
+        rclcpp::init(0, nullptr);
 
-  this->rosNode_ = std::make_shared<rclcpp::Node>("scoring_plugin_node");
-  this->scorePub_ = this->rosNode_->create_publisher<std_msgs::msg::Int32>("/score", 10);
+    this->rosNode_ = std::make_shared<rclcpp::Node>("scoring_plugin_node");
+    this->scorePub_ = this->rosNode_->create_publisher<std_msgs::msg::Int32>("/score", 10);
 }
 
-void ScoringPlugin::PostUpdate(const gz::sim::UpdateInfo &info,
-                               const gz::sim::EntityComponentManager &ecm)
+void ScoringPlugin::PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityComponentManager const &ecm)
 {
-  if (info.paused)
-    return;
+    if (info.paused)
+        return;
 
-  // Spin the ROS node so we can publish
-  rclcpp::spin_some(this->rosNode_);
+    // Spin the ROS node so we can publish
+    rclcpp::spin_some(this->rosNode_);
 
-  if (this->gateEntity_ == gz::sim::kNullEntity)
-  {
-    // Attempt to find the gate again, if needed
-    ecm.Each<gz::sim::components::Name>(
-      [&](const gz::sim::Entity &_entity, const gz::sim::components::Name *_nameComp)
-      {
-        if (_nameComp->Data() == this->gateModelName_)
-        {
-          this->gateEntity_ = _entity;
-          gzdbg << "[ScoringPlugin] Found gate entity in PostUpdate: " << _entity << std::endl;
-        }
-        return true;
-      }
-    );
-
-    // Gate not found yet
     if (this->gateEntity_ == gz::sim::kNullEntity)
     {
-      return;
+        // Attempt to find the gate again, if needed
+        ecm.Each<gz::sim::components::Name>(
+            [&](gz::sim::Entity const &_entity, gz::sim::components::Name const *_nameComp)
+            {
+                if (_nameComp->Data() == this->gateModelName_)
+                {
+                    this->gateEntity_ = _entity;
+                    gzdbg << "[ScoringPlugin] Found gate entity in PostUpdate: " << _entity << std::endl;
+                }
+                return true;
+            });
+
+        // Gate not found yet
+        if (this->gateEntity_ == gz::sim::kNullEntity)
+        {
+            return;
+        }
     }
-  }
 
-  // Get positions
-  auto subPoseComp  = ecm.Component<gz::sim::components::Pose>(this->subEntity_);
-  auto gatePoseComp = ecm.Component<gz::sim::components::Pose>(this->gateEntity_);
+    // Get positions
+    auto subPoseComp = ecm.Component<gz::sim::components::Pose>(this->subEntity_);
+    auto gatePoseComp = ecm.Component<gz::sim::components::Pose>(this->gateEntity_);
 
-  // Cannot proceed if either position is missing
-  if (!subPoseComp || !gatePoseComp)
-  {
-    return;
-  }
-  gz::math::Pose3d subPose  = subPoseComp->Data();
-  gz::math::Pose3d gatePose = gatePoseComp->Data();
-
-  // Detect pass
-  bool passed = this->SubPassedThroughGate(subPose, gatePose);
-  if (passed && !this->gatePassed_)
-  {
-    double DegreesDiff = getAngleBetweenVectors(subPose, gatePose);
-    if (DegreesDiff > 5.0)
+    // Cannot proceed if either position is missing
+    if (!subPoseComp || !gatePoseComp)
     {
-      this->score_ += 80;
-      std::cout << "awarding 80 points: angle penalty" << std::endl;
+        return;
     }
-    else
+    gz::math::Pose3d subPose = subPoseComp->Data();
+    gz::math::Pose3d gatePose = gatePoseComp->Data();
+
+    // Detect pass
+    bool passed = this->SubPassedThroughGate(subPose, gatePose);
+    if (passed && !this->gatePassed_)
     {
-      this->score_ += 100;
-      std::cout << "awarding 100 points: Perfect!" << std::endl;
+        double DegreesDiff = getAngleBetweenVectors(subPose, gatePose);
+        if (DegreesDiff > 5.0)
+        {
+            this->score_ += 80;
+            std::cout << "awarding 80 points: angle penalty" << std::endl;
+        }
+        else
+        {
+            this->score_ += 100;
+            std::cout << "awarding 100 points: Perfect!" << std::endl;
+        }
 
+        std::cout << "Sub passed through gate! Score: " << this->score_ << std::endl;
+        this->gatePassed_ = true;
+
+        // Publish score
+        std_msgs::msg::Int32 msg;
+        msg.data = this->score_;
+        this->scorePub_->publish(msg);
     }
-    
-    std::cout << "Sub passed through gate! Score: " << this->score_ << std::endl;
-    this->gatePassed_ = true;
-
-    // Publish score
-    std_msgs::msg::Int32 msg;
-    msg.data = this->score_;
-    this->scorePub_->publish(msg);
-  }
 }
 
-
-bool ScoringPlugin::SubPassedThroughGate(const gz::math::Pose3d &subPose,
-                                         const gz::math::Pose3d &gatePose)
+bool ScoringPlugin::SubPassedThroughGate(gz::math::Pose3d const &subPose, gz::math::Pose3d const &gatePose)
 {
-  // Very naive example: check distance
-  double dist = subPose.Pos().Distance(gatePose.Pos());
-  return (dist < 1.0);
+    // Very naive example: check distance
+    double dist = subPose.Pos().Distance(gatePose.Pos());
+    return (dist < 1.0);
 }
 
-//For getting angle between sub and gate
-double ScoringPlugin::getAngleBetweenVectors(const gz::math::Pose3d &subPose, const gz::math::Pose3d &gatePose)
+// For getting angle between sub and gate
+double ScoringPlugin::getAngleBetweenVectors(gz::math::Pose3d const &subPose, gz::math::Pose3d const &gatePose)
 {
+    // getting position relative to world
+    gz::math::Vector3d gateForward = gatePose.Rot().RotateVector(gz::math::Vector3d::UnitX);
+    gz::math::Vector3d subForward = subPose.Rot().RotateVector(gz::math::Vector3d::UnitX);
 
-  // getting position relative to world
-  gz::math::Vector3d gateForward = gatePose.Rot().RotateVector(gz::math::Vector3d::UnitX);
-  gz::math::Vector3d subForward = subPose.Rot().RotateVector(gz::math::Vector3d::UnitX);
- 
-  double dot = gateForward.Dot(subForward);
-  double mag = gateForward.Length() * subForward.Length();
-  if (mag < 1e-6)
-    return 0.0;
- 
-   double cosAngle = dot / mag;
-   if (cosAngle > 1 || cosAngle < -1)
-   {
-    std::cout << "Possible Rounding Error with cosAngle" << std::endl; 
-    return 0.0;
-   }
-   double angleRad = std::acos(cosAngle);
-   double angleDeg = angleRad * 180.0 / 3.14159265;
-   return angleDeg;
+    double dot = gateForward.Dot(subForward);
+    double mag = gateForward.Length() * subForward.Length();
+    if (mag < 1e-6)
+        return 0.0;
+
+    double cosAngle = dot / mag;
+    if (cosAngle > 1 || cosAngle < -1)
+    {
+        std::cout << "Possible Rounding Error with cosAngle" << std::endl;
+        return 0.0;
+    }
+    double angleRad = std::acos(cosAngle);
+    double angleDeg = angleRad * 180.0 / 3.14159265;
+    return angleDeg;
 }
 
-} // namespace scoring_plugin
-
-
+}  // namespace scoring_plugin
