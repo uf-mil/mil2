be19c7f1b0fa0c427ec66626e353d012900dc25a
Daniel Parra <daniel27parra@gmail.com>
2025-03-29 18:43:22 +0000

    ran precommit on code

--- src/mil_common/mil_preflight/include/mil_preflight/uis/ftxui/testsPage.h
+++ src/mil_common/mil_preflight/include/mil_preflight/uis/ftxui/testsPage.h
@@ -14,29 +14,29 @@ using namespace ftxui;
 
 class TestsPage : public ComponentBase, public Job
 {
-public:
-  TestsPage(std::string const& filePath);
-  ~TestsPage();
+  public:
+    TestsPage(std::string const& filePath);
+    ~TestsPage();
 
-private:
-  Component tabsContainer_;
-  Component pagesContainer_;
-  Component main_;
+  private:
+    Component tabsContainer_;
+    Component pagesContainer_;
+    Component main_;
 
-  int selector_ = 0;
-  bool selectAll_ = false;
-  std::atomic<size_t> currentTest_ = 0;
-  std::atomic<bool> running_ = false;
+    int selector_ = 0;
+    bool selectAll_ = false;
+    std::atomic<size_t> currentTest_ = 0;
+    std::atomic<bool> running_ = false;
 
-  std::string const buttonLabels_[4] = { " Run ", " ·   ", "  ·  ", "   · " };
-  size_t ticker_ = 0;
+    std::string const buttonLabels_[4] = { " Run ", " ·   ", "  ·  ", "   · " };
+    size_t ticker_ = 0;
 
-  std::optional<std::reference_wrapper<Test>> nextTest() final;
-  std::optional<std::reference_wrapper<Test>> createTest(std::string&& name, std::string&& plugin) final;
-  void onFinish(Job::Report&& report) final;
+    std::optional<std::reference_wrapper<Test>> nextTest() final;
+    std::optional<std::reference_wrapper<Test>> createTest(std::string&& name, std::string&& plugin) final;
+    void onFinish(Job::Report&& report) final;
 
-  Element Render();
-  bool OnEvent(Event event) final;
+    Element Render();
+    bool OnEvent(Event event) final;
 };
 
 }  // namespace mil_preflight
