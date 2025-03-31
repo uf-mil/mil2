8d55241ac3ac2a8dffb3174b9f3f629de4c4e45b
tjcanro <tjcanro@gmail.com>
2025-03-30 22:47:00 -0400

    Completed Alarm proxy with testing

---
+++ src/mil_common/ros2_alarms/scripts/bash_aliases.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script is meant to be sourced when using the ros2_alarms package.
+
+alias visual_test="colcon test --packages-select ros2_alarms --event-handler=console_direct+"
