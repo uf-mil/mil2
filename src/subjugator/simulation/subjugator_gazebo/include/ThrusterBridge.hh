5e3aabc5cf7b99af443c0a72c9200e4eb7314c5d
CarterKreis <cckreis0507@gmail.com>
2025-04-04 15:43:01 -0400

    Added basic steps for ROS to GZ bridge. ROS nodes dont seem to be built and callback function is not working

--- src/subjugator/simulation/subjugator_gazebo/include/ThrusterBridge.hh
+++ src/subjugator/simulation/subjugator_gazebo/include/ThrusterBridge.hh
@@ -23,8 +23,9 @@ using std::placeholders::_1;
 
 namespace thrusterBridge
 {
-class ThrusterBridge : public gz::sim::System, public gz::sim::ISystemConfigure, public gz::sim::ISystemPostUpdate
-
+class ThrusterBridge : public gz::sim::System,
+                       public gz::sim::ISystemConfigure,
+                       public gz::sim::ISystemPostUpdate
 {
   public:
     ThrusterBridge();
