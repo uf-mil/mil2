28ce2b27fb601b9838ac5d22e810a4677bd6e498
CarterKreis <cckreis0507@gmail.com>
2025-04-04 15:43:01 -0400

    Added basic steps for ROS to GZ bridge. ROS nodes dont seem to be built and callback function is not working

--- src/subjugator/simulation/subjugator_gazebo/include/ThrusterBridge.hh
+++ src/subjugator/simulation/subjugator_gazebo/include/ThrusterBridge.hh
@@ -1,5 +1,5 @@
-#ifndef HYDROPHONE__HH_
-#define HYDROPHONE__HH_
+#ifndef THRUSTERBRIDGE__HH_
+#define THRUSTERBRIDGE__HH_
 
 #include <iostream>
 
@@ -7,64 +7,64 @@
 
 #include <gz/math/Pose3.hh>  // For gz::math::Pose3d
 #include <gz/sim/EntityComponentManager.hh>
+#include <gz/sim/EventManager.hh>
 #include <gz/sim/InstallationDirectories.hh>
 #include <gz/sim/Model.hh>
 #include <gz/sim/System.hh>  // For gz::sim::System
 #include <gz/sim/World.hh>
 #include <gz/sim/components/Name.hh>  // For gz::sim::components::Name
 #include <gz/sim/components/Pose.hh>  // For gz::sim::components::Pose
+#include <gz/transport/Node.hh>       // For gz::transport::Node
 #include <sdf/Element.hh>
 #include <sdf/World.hh>
 #include <sdf/sdf.hh>
 
-namespace hydrophone
+// The .msg file is named ProcessedPing.msg (PascalCase) but #include must be the name in snake_case
+#include "../../../subjugator_msgs/msg/thruster_efforts.hpp"
+#include "subjugator_msgs/msg/thruster_efforts.hpp"
+
+using std::placeholders::_1;
+
+namespace thrusterBridge
 {
-class Hydrophone : public gz::sim::System, public gz::sim::ISystemConfigure, public gz::sim::ISystemPostUpdate
+class ThrusterBridge : public gz::sim::System,
+                       public gz::sim::ISystemConfigure,
+                       public gz::sim::ISystemPostUpdate,
+                       public rclcpp::Node
 {
   public:
-    Hydrophone();
-    ~Hydrophone();
+    ThrusterBridge();
+    ~ThrusterBridge();
 
-    // Configure() - Gathers Info at the start of the simulation //
+    // Configure() -  //
     void Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
                    gz::sim::EntityComponentManager &ecm, gz::sim::EventManager &eventMgr) override;
 
-    // FormatPinger() - Extract frequency and name //
-    void FormatPinger(std::string &entityName);
-
-    // System PostUpdate - Called every simulation step to update pinger/hydrophone distances //
+    // System PostUpdate - //
     void PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityComponentManager const &ecm) override;
 
-    // GatherWorldInfo() - Scrape info from the worldSDF to get pinger info //
-    void GatherWorldInfo(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
-                         gz::sim::EntityComponentManager const &ecm);
-
   private:
-    // Boolean so GatherWorldInfo is only called once //
-    bool worldGathered = false;
-
-    // Stored Entities //
-    gz::sim::Entity modelEntity_{ gz::sim::kNullEntity };       // World Entity
-    gz::sim::Entity hydrophoneEntity_{ gz::sim::kNullEntity };  // Hydrophone Entity
-
-    // World SDF //
-    std::shared_ptr<sdf::Element const> sdf_;
-
     // ROS node + publisher //
     std::shared_ptr<rclcpp::Node> rosNode_;
-    rclcpp::Publisher<mil_msgs::msg::ProcessedPing>::SharedPtr pingPub_;
+    rclcpp::Publisher<subjugator_msgs::msg::ThrusterEfforts>::SharedPtr thrusterSub_;
 
-    // Vectors for Pinger Info //
-    std::vector<std::string> pingerNames_;
-    std::vector<gz::math::Pose3d> pingerLocations_;
-    std::vector<double> pingerFrequencies_;
+    // Gazebo Thruster Node //
+    gz::transport::Node gz_node;
 
-    // Sub9 Pose //
-    gz::math::Pose3d hydrophonePose_{ gz::math::Pose3d::Zero };
+    // Callback for receiving thruster efforts //
+    void receiveEffortCallback(subjugator_msgs::msg::ThrusterEfforts const &msg);
 
-    // Difference Vectors - Where the pinger is relative to the hydrophone
-    std::vector<gz::math::Vector3d> pingerDiffs_;
+    // Thruster efforts storage - FLV = Front Left Vertical, BRH = Back Right Horizontal //
+    std::vector<float> thrusterEfforts_;
+    float flh;
+    float frh;
+    float blh;
+    float brh;
+    float flv;
+    float frv;
+    float blv;
+    float brv;
 };
 
-}  // namespace hydrophone
-#endif  // HYDROPHONE_HH
+}  // namespace thrusterBridge
+#endif  // THRUSTERBRIDGE__HH_
