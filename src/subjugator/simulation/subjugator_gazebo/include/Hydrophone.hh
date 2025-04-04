165797e8c07588f629c43f516cb331d9c64e4832
CarterKreis <cckreis0507@gmail.com>
2025-04-04 12:26:42 -0400

    Im keithing it

--- src/subjugator/simulation/subjugator_gazebo/include/Hydrophone.hh
+++ src/subjugator/simulation/subjugator_gazebo/include/Hydrophone.hh
@@ -2,6 +2,12 @@
 #define HYDROPHONE__HH_
 
 #include <chrono>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <rclcpp/rclcpp.hpp>
+
 #include <gz/math/Pose3.hh>  // For gz::math::Pose3d
 #include <gz/sim/EntityComponentManager.hh>
 #include <gz/sim/InstallationDirectories.hh>
@@ -10,13 +16,9 @@
 #include <gz/sim/World.hh>
 #include <gz/sim/components/Name.hh>  // For gz::sim::components::Name
 #include <gz/sim/components/Pose.hh>  // For gz::sim::components::Pose
-#include <iostream>
-#include <rclcpp/rclcpp.hpp>
 #include <sdf/Element.hh>
 #include <sdf/World.hh>
 #include <sdf/sdf.hh>
-#include <string>
-#include <vector>
 
 // The .msg file is named ProcessedPing.msg (PascalCase) but #include must be the name in snake_case
 #include "mil_msgs/msg/processed_ping.hpp"
@@ -25,49 +27,49 @@ namespace hydrophone
 {
 class Hydrophone : public gz::sim::System, public gz::sim::ISystemConfigure, public gz::sim::ISystemPostUpdate
 {
-public:
-  Hydrophone();
-  ~Hydrophone();
+  public:
+    Hydrophone();
+    ~Hydrophone();
 
-  // Configure() - Gathers Info at the start of the simulation //
-  void Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
-                 gz::sim::EntityComponentManager &ecm, gz::sim::EventManager &eventMgr) override;
+    // Configure() - Gathers Info at the start of the simulation //
+    void Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
+                   gz::sim::EntityComponentManager &ecm, gz::sim::EventManager &eventMgr) override;
 
-  // FormatPinger() - Extract frequency and name //
-  void FormatPinger(std::string &entityName);
+    // FormatPinger() - Extract frequency and name //
+    void FormatPinger(std::string &entityName);
 
-  // System PostUpdate - Called every simulation step to update pinger/hydrophone distances //
-  void PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityComponentManager const &ecm) override;
+    // System PostUpdate - Called every simulation step to update pinger/hydrophone distances //
+    void PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityComponentManager const &ecm) override;
 
-  // GatherWorldInfo() - Scrape info from the worldSDF to get pinger info //
-  void GatherWorldInfo(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
-                       gz::sim::EntityComponentManager const &ecm);
+    // GatherWorldInfo() - Scrape info from the worldSDF to get pinger info //
+    void GatherWorldInfo(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
+                         gz::sim::EntityComponentManager const &ecm);
 
-private:
-  // Boolean so GatherWorldInfo is only called once //
-  bool worldGathered = false;
+  private:
+    // Boolean so GatherWorldInfo is only called once //
+    bool worldGathered = false;
 
-  // Stored Entities //
-  gz::sim::Entity modelEntity_{ gz::sim::kNullEntity };       // World Entity
-  gz::sim::Entity hydrophoneEntity_{ gz::sim::kNullEntity };  // Hydrophone Entity
+    // Stored Entities //
+    gz::sim::Entity modelEntity_{ gz::sim::kNullEntity };       // World Entity
+    gz::sim::Entity hydrophoneEntity_{ gz::sim::kNullEntity };  // Hydrophone Entity
 
-  // World SDF //
-  std::shared_ptr<sdf::Element const> sdf_;
+    // World SDF //
+    std::shared_ptr<sdf::Element const> sdf_;
 
-  // ROS node + publisher //
-  std::shared_ptr<rclcpp::Node> rosNode_;
-  rclcpp::Publisher<mil_msgs::msg::ProcessedPing>::SharedPtr pingPub_;
+    // ROS node + publisher //
+    std::shared_ptr<rclcpp::Node> rosNode_;
+    rclcpp::Publisher<mil_msgs::msg::ProcessedPing>::SharedPtr pingPub_;
 
-  // Vectors for Pinger Info //
-  std::vector<std::string> pingerNames_;
-  std::vector<gz::math::Pose3d> pingerLocations_;
-  std::vector<double> pingerFrequencies_;
+    // Vectors for Pinger Info //
+    std::vector<std::string> pingerNames_;
+    std::vector<gz::math::Pose3d> pingerLocations_;
+    std::vector<double> pingerFrequencies_;
 
-  // Sub9 Pose //
-  gz::math::Pose3d hydrophonePose_{ gz::math::Pose3d::Zero };
+    // Sub9 Pose //
+    gz::math::Pose3d hydrophonePose_{ gz::math::Pose3d::Zero };
 
-  // Difference Vectors - Where the pinger is relative to the hydrophone
-  std::vector<gz::math::Vector3d> pingerDiffs_;
+    // Difference Vectors - Where the pinger is relative to the hydrophone
+    std::vector<gz::math::Vector3d> pingerDiffs_;
 };
 
 }  // namespace hydrophone
--- src/subjugator/simulation/subjugator_gazebo/src/Hydrophone.cc
+++ src/subjugator/simulation/subjugator_gazebo/src/Hydrophone.cc
@@ -1,8 +1,8 @@
 #include "Hydrophone.hh"
 
-#include <gz/common/Console.hh>
-
 #include "gz/plugin/Register.hh"  // For GZ_ADD_PLUGIN
+
+#include <gz/common/Console.hh>
 
 namespace hydrophone
 {
@@ -22,26 +22,26 @@ Hydrophone::~Hydrophone()
 // It then stores the frequency and name in their respective vectors.
 void Hydrophone::FormatPinger(std::string &entityName)
 {
-  // Input string is of format: Pinger_1X_00000/Pinger_2X_00000
-  // Where X can be a value from A-Z and 00000 is a 5 digit frequency
+    // Input string is of format: Pinger_1X_00000/Pinger_2X_00000
+    // Where X can be a value from A-Z and 00000 is a 5 digit frequency
 
-  // Split input string by underscore
-  size_t lastUnderscore = entityName.find_last_of('_');
+    // Split input string by underscore
+    size_t lastUnderscore = entityName.find_last_of('_');
 
-  // Extract the frequency after the last underscore and convert to an integer
-  std::string frequencyStr = entityName.substr(lastUnderscore + 1);
-  int frequency = std::stoi(frequencyStr);
+    // Extract the frequency after the last underscore and convert to an integer
+    std::string frequencyStr = entityName.substr(lastUnderscore + 1);
+    int frequency = std::stoi(frequencyStr);
 
-  // Extract the name of the Pinger
-  std::string pingerName = entityName.substr(0, lastUnderscore);
+    // Extract the name of the Pinger
+    std::string pingerName = entityName.substr(0, lastUnderscore);
 
-  // Input Frequency and Name to the respective vectors
-  this->pingerFrequencies_.push_back(frequency);
-  this->pingerNames_.push_back(pingerName);
+    // Input Frequency and Name to the respective vectors
+    this->pingerFrequencies_.push_back(frequency);
+    this->pingerNames_.push_back(pingerName);
 
-  // Print the extracted frequency and name
-  // std::cout << "[Hydrophone] Extracted Frequency: " << frequency << std::endl;
-  // std::cout << "[Hydrophone] Extracted Name: " << pingerName << std::endl;
+    // Print the extracted frequency and name
+    // std::cout << "[Hydrophone] Extracted Frequency: " << frequency << std::endl;
+    // std::cout << "[Hydrophone] Extracted Name: " << pingerName << std::endl;
 }
 
 // Configure() - Gathers modelEntity and sdf at the start of the simulation //
@@ -49,21 +49,21 @@ void Hydrophone::FormatPinger(std::string &entityName)
 void Hydrophone::Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
                            gz::sim::EntityComponentManager &ecm, gz::sim::EventManager &eventMgr)
 {
-  // Gather necessary information for future GatherWorldInfo() call
-  this->modelEntity_ = entity;
+    // Gather necessary information for future GatherWorldInfo() call
+    this->modelEntity_ = entity;
 
-  // Create copy of the SDF element to perform modifications due to const
-  this->sdf_ = sdf->Clone();
+    // Create copy of the SDF element to perform modifications due to const
+    this->sdf_ = sdf->Clone();
 
-  // Initialize the ROS node and publisher
-  if (!rclcpp::ok())
-  {
-    rclcpp::init(0, nullptr);
-  }
+    // Initialize the ROS node and publisher
+    if (!rclcpp::ok())
+    {
+        rclcpp::init(0, nullptr);
+    }
 
-  // Create a ROS2 node for publishing
-  this->rosNode_ = std::make_shared<rclcpp::Node>("hydrophone_node");
-  this->pingPub_ = this->rosNode_->create_publisher<mil_msgs::msg::ProcessedPing>("/ping", 1);
+    // Create a ROS2 node for publishing
+    this->rosNode_ = std::make_shared<rclcpp::Node>("hydrophone_node");
+    this->pingPub_ = this->rosNode_->create_publisher<mil_msgs::msg::ProcessedPing>("/ping", 1);
 }
 
 // GatherWorldInfo() - At start of simulation scrape info from the worldSDF to get pinger info //
@@ -75,54 +75,54 @@ void Hydrophone::Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::E
 void Hydrophone::GatherWorldInfo(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
                                  gz::sim::EntityComponentManager const &_ecm)
 {
-  // String to check for pinger names
-  std::string pingerPrefix = "Pinger_";
+    // String to check for pinger names
+    std::string pingerPrefix = "Pinger_";
 
-  // Number of entities in the worldSDF
-  size_t entityCount = _ecm.EntityCount();
-  // std::cout << "Entity Count: " << entityCount << std::endl;
+    // Number of entities in the worldSDF
+    size_t entityCount = _ecm.EntityCount();
+    // std::cout << "Entity Count: " << entityCount << std::endl;
 
-  // Iterate through all entities and pass in Pose and Name components
-  // to check for pinger names and store their poses
-  _ecm.Each<gz::sim::components::Pose, gz::sim::components::Name>(
-      [&](gz::sim::Entity const &ent, gz::sim::components::Pose const *pose,
-          gz::sim::components::Name const *name) -> bool
-      {
-        // Check if pose and name components are valid
-        if (!pose || !name)
+    // Iterate through all entities and pass in Pose and Name components
+    // to check for pinger names and store their poses
+    _ecm.Each<gz::sim::components::Pose, gz::sim::components::Name>(
+        [&](gz::sim::Entity const &ent, gz::sim::components::Pose const *pose,
+            gz::sim::components::Name const *name) -> bool
         {
-          return true;
+            // Check if pose and name components are valid
+            if (!pose || !name)
+            {
+                return true;
+            }
+
+            // Get current entity name
+            std::string entityName = name->Data();
+            // std::cout << "Entity Name: " << entityName << std::endl;
+
+            // If entity is hydrophone, store its entity
+            if (entityName == "hydrophone_sensor_link")
+            {
+                this->hydrophoneEntity_ = ent;
+            }
+
+            // Check if name starts with "Pinger_, i.e. if it is a pinger"
+            if (entityName.find(pingerPrefix) == 0)
+            {
+                // Store the pose of the pinger
+                this->pingerLocations_.push_back(pose->Data());
+
+                // std::cout << "Pinger Pose X: " << pose->Data().Pos().X() << std::endl;
+                // std::cout << "Pinger Pose Y: " << pose->Data().Pos().Y() << std::endl;
+                // std::cout << "Pinger Pose Z: " << pose->Data().Pos().Z() << std::endl;
+
+                // Format the name to extract frequency and Name
+                FormatPinger(entityName);
+            }
+
+            // Continue to next entity
+            return true;
         }
 
-        // Get current entity name
-        std::string entityName = name->Data();
-        // std::cout << "Entity Name: " << entityName << std::endl;
-
-        // If entity is hydrophone, store its entity
-        if (entityName == "hydrophone_sensor_link")
-        {
-          this->hydrophoneEntity_ = ent;
-        }
-
-        // Check if name starts with "Pinger_, i.e. if it is a pinger"
-        if (entityName.find(pingerPrefix) == 0)
-        {
-          // Store the pose of the pinger
-          this->pingerLocations_.push_back(pose->Data());
-
-          // std::cout << "Pinger Pose X: " << pose->Data().Pos().X() << std::endl;
-          // std::cout << "Pinger Pose Y: " << pose->Data().Pos().Y() << std::endl;
-          // std::cout << "Pinger Pose Z: " << pose->Data().Pos().Z() << std::endl;
-
-          // Format the name to extract frequency and Name
-          FormatPinger(entityName);
-        }
-
-        // Continue to next entity
-        return true;
-      }
-
-  );  // End of Each() function
+    );  // End of Each() function
 
 }  // End of GatherWorldInfo()
 
@@ -134,61 +134,61 @@ void Hydrophone::GatherWorldInfo(gz::sim::Entity const &entity, std::shared_ptr<
 // 3. Publishes the message to the /ping topic
 void Hydrophone::PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityComponentManager const &ecm)
 {
-  //  Check if the simulation is paused or running
-  if (info.paused)
-  {
-    return;
-  }
+    //  Check if the simulation is paused or running
+    if (info.paused)
+    {
+        return;
+    }
 
-  // Scrape worldSDF for pinger(s) info once
-  if (!worldGathered)
-  {
-    GatherWorldInfo(this->modelEntity_, this->sdf_, ecm);
-    worldGathered = true;
-  }
+    // Scrape worldSDF for pinger(s) info once
+    if (!worldGathered)
+    {
+        GatherWorldInfo(this->modelEntity_, this->sdf_, ecm);
+        worldGathered = true;
+    }
 
-  // Spin the ROS node to process incoming messages
-  rclcpp::spin_some(this->rosNode_);
+    // Spin the ROS node to process incoming messages
+    rclcpp::spin_some(this->rosNode_);
 
-  // Get current pose of the Sub9 entity
-  auto hydrophoneComponent = ecm.Component<gz::sim::components::Pose>(this->hydrophoneEntity_);
-  if (hydrophoneComponent)
-  {
-    this->hydrophonePose_ = hydrophoneComponent->Data();
-    // std::cout << "[Hydrophone] Hydrophone Pose Updated: " << this->hydrophonePose_ << std::endl;
-  }
+    // Get current pose of the Sub9 entity
+    auto hydrophoneComponent = ecm.Component<gz::sim::components::Pose>(this->hydrophoneEntity_);
+    if (hydrophoneComponent)
+    {
+        this->hydrophonePose_ = hydrophoneComponent->Data();
+        // std::cout << "[Hydrophone] Hydrophone Pose Updated: " << this->hydrophonePose_ << std::endl;
+    }
 
-  // Iterate over all pinger locations and compute difference vectors
-  int i = 0;
-  for (auto const &pingerPose : this->pingerLocations_)
-  {
-    // Compute difference vector (pinger - hydrophone)
-    gz::math::Vector3d diffVector = pingerPose.Pos() - hydrophonePose_.Pos();
-    this->pingerDiffs_.push_back(diffVector);
+    // Iterate over all pinger locations and compute difference vectors
+    int i = 0;
+    for (auto const &pingerPose : this->pingerLocations_)
+    {
+        // Compute difference vector (pinger - hydrophone)
+        gz::math::Vector3d diffVector = pingerPose.Pos() - hydrophonePose_.Pos();
+        this->pingerDiffs_.push_back(diffVector);
 
-    // Create ROS2 message
-    // Msg for Origin Direction Body
-    mil_msgs::msg::ProcessedPing msg;
-    msg.origin_direction_body.x = diffVector.X();
-    msg.origin_direction_body.y = diffVector.Y();
-    msg.origin_direction_body.z = diffVector.Z();
+        // Create ROS2 message
+        // Msg for Origin Direction Body
+        mil_msgs::msg::ProcessedPing msg;
+        msg.origin_direction_body.x = diffVector.X();
+        msg.origin_direction_body.y = diffVector.Y();
+        msg.origin_direction_body.z = diffVector.Z();
 
-    // Msg for Pinger Frequency
-    msg.frequency = this->pingerFrequencies_.at(i);
+        // Msg for Pinger Frequency
+        msg.frequency = this->pingerFrequencies_.at(i);
 
-    // Msg for Origin Distance
-    msg.origin_distance_m = diffVector.Length();
+        // Msg for Origin Distance
+        msg.origin_distance_m = diffVector.Length();
 
-    // Publish message
-    this->pingPub_->publish(msg);
+        // Publish message
+        this->pingPub_->publish(msg);
 
-    // std::cout << "Pinger at [" << pingerPose.Pos() << "]" << std::endl
-    //           << "Difference vector: [" << diffVector << "]" << std::endl
-    //           << "Difference Length: [" << diffVector.Length() << "]" << std::endl
-    //           << "Frequency: [" << this->pingerFrequencies_.at(i) << "]" << std::endl;
+        // std::cout << "Pinger at [" << pingerPose.Pos() << "]" << std::endl
+        //           << "Difference vector: [" << diffVector << "]" << std::endl
+        //           << "Difference Length: [" << diffVector.Length() << "]" << std::endl
+        //           << "Frequency: [" << this->pingerFrequencies_.at(i) << "]" << std::endl;
 
-    i++;
-  }
+        i++;
+    }
 }
 
 }  // namespace hydrophone
