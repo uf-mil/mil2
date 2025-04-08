c55b20d350696c8365fd2d8ff2b079a6f997d224
Keith Khadar <keith.khadar@gmail.com>
2025-04-08 19:45:17 +0000

    final fix

--- scripts/slam_install.sh
+++ scripts/slam_install.sh
@@ -7,7 +7,7 @@ sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavform
 sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev libjasper-dev -y
 
 sudo apt-get install libglew-dev libboost-all-dev libssl-dev -y
-
+sudo apt-get install libepoxy -y
 sudo apt install libeigen3-dev -y
 
 # Install OpenCV
@@ -19,7 +19,7 @@ sudo apt install libopencv-dev -y
 cd ../ext
 git clone --recursive https://github.com/stevenlovegrove/Pangolin.git
 cd Pangolin
-./scripts/install_prerequisites.sh --dry-run recommended
+./scripts/install_prerequisites.sh recommended
 
 # Configure and build
 cmake -B build
