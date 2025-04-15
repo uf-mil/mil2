ba2e87dae5cd92d20ccb5492c435e41f1a9796e7
Alex Johnson <alexjohnson@ufl.edu>
2025-04-15 18:16:49 -0400

    working on subjugator reset and enable services

--- src/subjugator/gnc/subjugator_controller/src/controller.cpp
+++ src/subjugator/gnc/subjugator_controller/src/controller.cpp
@@ -4,7 +4,9 @@ PIDController::PIDController() : Node("pid_controller")
 {
     this->is_shutdown = false;
     this->heard_odom = false;
+    this->is_enabled = true;  // TODO false by default
 
+    // create subscriptions and publishers
     sub_odom_ = this->create_subscription<nav_msgs::msg::Odometry>(
         "odometry/filtered", 10, [this](nav_msgs::msg::Odometry::UniquePtr msg) { this->odom_cb(std::move(msg)); });
     sub_goal_trajectory_ = this->create_subscription<geometry_msgs::msg::Pose>(
@@ -15,13 +17,19 @@ PIDController::PIDController() : Node("pid_controller")
         [this](geometry_msgs::msg::Pose::UniquePtr msg) { this->relative_goal_trajectory_cb(std::move(msg)); });
     pub_cmd_wrench_ = this->create_publisher<geometry_msgs::msg::Wrench>("cmd_wrench", rclcpp::QoS(1).reliable());
 
+    // create reset service
+    this->reset_service_ = this->create_service<std_srvs::srv::Empty>(
+        "reset_controller",
+        [this](std::shared_ptr<std_srvs::srv::Empty::Request> const request,
+               std::shared_ptr<std_srvs::srv::Empty::Response> response) { this->reset(request, response); });
+
     // callback to send 0 cmd_wrench on shutdown
     using rclcpp::contexts::get_global_default_context;
     get_global_default_context()->add_pre_shutdown_callback(
         [this]()
         {
             this->is_shutdown = true;
-            this->shutdown();
+            this->publish_zero_command();
         });
 
     param_subscriber_ = std::make_shared<rclcpp::ParameterEventHandler>(this);
@@ -69,6 +77,11 @@ PIDController::PIDController() : Node("pid_controller")
     last_goal_trajectory_ = last_odom_;
     last_cmd_time_ = this->get_clock()->now();
 
+    if (!this->is_enabled)
+    {
+        RCLCPP_INFO(this->get_logger(), "PID controller is disabled. Call enable_controller service to start.");
+    }
+
     control_loop();
 }
 
@@ -77,6 +90,11 @@ void PIDController::control_loop()
     rclcpp::Rate rate(10);
     while (rclcpp::ok() && !this->is_shutdown)
     {
+        if (!is_enabled)
+        {
+            continue;
+        }
+
         RCLCPP_INFO(this->get_logger(), "control loop");
 
         rclcpp::Time tnow = this->get_clock()->now();
@@ -182,10 +200,46 @@ void PIDController::relative_goal_trajectory_cb(geometry_msgs::msg::Pose::Unique
                 std::to_string(relative_goal(6)).c_str());
 }
 
-void PIDController::shutdown()
+void PIDController::publish_zero_command()
 {
     std::array<double, 6> commands = { 0 };
     publish_commands(commands);
     pub_cmd_wrench_->wait_for_all_acked();
-    RCLCPP_INFO(this->get_logger(), "Shutting down PID controller, sent 0 cmd_wrench.");
+    RCLCPP_INFO(this->get_logger(), "Sending 0 cmd_wrench.");
+}
+
+void PIDController::reset(std::shared_ptr<std_srvs::srv::Empty::Request> const request,
+                          std::shared_ptr<std_srvs::srv::Empty::Response> response)
+{
+    RCLCPP_INFO(this->get_logger(), "Resetting PID controller.");
+
+    // publish zero thrust (safety yay)
+    this->publish_zero_command();
+
+    // update gains
+    std::vector<std::string> const params = { "kp", "ki", "kd", "imax", "imin", "antiwindup" };
+    for (auto const &param : params)
+    {
+        param_map_[param].first = this->get_parameter(param).as_double_array();
+    }
+    for (size_t i = 0; i < pid_vec_.size(); i++)
+    {
+        pid_vec_[i].set_gains(param_map_["kp"].first[i], param_map_["ki"].first[i], param_map_["kd"].first[i],
+                              param_map_["imax"].first[i], param_map_["imin"].first[i],
+                              param_map_["antiwindup"].first[i]);
+    }
+
+    // wait to hear odom msg
+    RCLCPP_INFO(this->get_logger(), "Waiting for odometry msg...");
+    while (!this->heard_odom)
+    {
+        rclcpp::spin_some(this->get_node_base_interface());
+    }
+    RCLCPP_INFO(this->get_logger(), "Heard odometry msg. Starting control loop.");
+
+    // set starting command to current odom msg (stationkeep)
+    last_goal_trajectory_ = last_odom_;
+    last_cmd_time_ = this->get_clock()->now();
+
+    control_loop();
 }
