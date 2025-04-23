14c807a19a587e051f17ea4f3682a65f922449e4
Cameron Brown <me@cbrxyz.com>
2025-04-23 14:23:30 -0400

    subjugator_path_planner: add global path planner for subjugator using splines

---
+++ src/subjugator/gnc/subjugator_path_planner/src/node.cpp
@@ -0,0 +1,9 @@
+#include "subjugator_path_planner/PathPlanner.hpp"
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<PathPlanner>());
+    rclcpp::shutdown();
+    return 0;
+}
