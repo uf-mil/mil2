dec6d26e7afcc372e24c1a7dd5b9d280cb0f36fa
Cameron Brown <me@cbrxyz.com>
2025-04-25 13:27:20 -0400

    scripts/setup.bash: Add tidy bash function for running clang-tidy

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,3 +1,4 @@
+#! /usr/bin/env bash
 MIL_REPO="$HOME/mil2"
 
 if [[ $(ps -p $$ | tail -n 1 | awk '{ print $4 }') == "zsh" ]]; then
@@ -118,10 +119,90 @@ _cb_autocomplete() {
 	while IFS='' read -r line; do COMPREPLY+=("$line"); done < <(compgen -W "$packages" -- "$current_phrase")
 }
 
+# Used to run clang-tidy on packages
+# Usage:
+#   tidy                  --> Nothing
+#   tidy <package>        --> Specific package
+#   tidy <file>           --> Specific file
+#   tidy --all            --> All packages
+#   tidy --no-stop        --> Go through all errors before stopping$
+#   tidy --fix            --> Fix changes
+#   tidy --diff-files     --> Look at files which have changed in git
+tidy() {
+	local packages=""
+	local files=()
+	local clang_tidy_args=()
+	local dont_stop=false
+	for arg in "$@"; do
+		if [ "$arg" = "--no-stop" ]; then
+			dont_stop=true
+		elif [ "$arg" = "--all" ]; then
+			packages=$(colcon list --names-only --base-paths $MIL_REPO)
+		elif [ "$arg" = "--fix" ]; then
+			clang_tidy_args+=("--fix")
+		elif [ "$arg" = "--verbose" ]; then
+			clang_tidy_args+=("--verbose")
+		elif [ "$arg" = "--help" ]; then
+			echo "Usage: tidy [--all] [--fix] [--verbose] <package>"
+			return
+		elif [ "$arg" = "--diff-files" ]; then
+			# Get the list of modified files in the git repository
+			local modified_files=()
+			while IFS='' read -r line; do modified_files+=("$line"); done < <(git diff --name-only HEAD)
+			# Filter the files to only include .cpp, .h, and .hpp files
+			for file in "${modified_files[@]}"; do
+				if [[ $file == *.cpp || $file == *.h || $file == *.hpp ]]; then
+					echo ">>> found modified file: $file"
+					files+=("$file")
+				fi
+			done
+		elif [[ $arg == -* ]]; then
+			echo "Unknown option: $arg"
+			return 1
+		else
+			packages+=("$arg")
+		fi
+	done
+
+	# Run clang-tidy on each package
+	for package in "${packages[@]}"; do
+		echo ">>> Running clang-tidy on $package..."
+		local package_path
+		package_path=$(colcon list --base-paths $MIL_REPO --packages-select "$package" --paths-only)
+		# all
+		if $dont_stop; then
+			clang-tidy-20 "${clang_tidy_args[@]}" -p "$MIL_REPO/build/$package" "$(fd --type f --extension cpp --extension h --extension hpp . "$package_path")"
+		else
+			# only first error
+			package_files=()
+			while IFS='' read -r line; do package_files+=("$line"); done < <(fd --type f --extension cpp --extension h --extension hpp . "$package_path")
+			for file in "${package_files[@]}"; do
+				echo ">>> Running clang-tidy on $file..."
+				clang-tidy-20 "${clang_tidy_args[@]}" -p "$MIL_REPO/build/$package" "$file"
+				if [ $? -ne 0 ]; then
+					echo ">>> Stopping on first error in $file"
+					break
+				fi
+			done
+		fi
+	done
+
+	# Run clang-tidy on each file
+	for file in "${files[@]}"; do
+		echo ">>> Running clang-tidy on $file..."
+		clang-tidy-20 "${clang_tidy_args[@]}" -p "$MIL_REPO/build" "$file"
+		if [ $? -ne 0 ]; then
+			echo ">>> Stopping on first error in $file"
+			break
+		fi
+	done
+}
+
 # bash autocompletion
 complete -F _cb_autocomplete cb
 complete -F _cb_autocomplete colcon_cd
 complete -F _cb_autocomplete ccd
+complete -F _cb_autocomplete tidy
 
 # Print all devices on the specified subnet / network prefix
 list_lan_devices() {
