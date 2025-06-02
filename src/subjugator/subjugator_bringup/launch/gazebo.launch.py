16837d5273ba5c794695f94ffeadf21d8ba309a1
Carlos Chavez <cforcarlos22@gmail.com>
2025-06-02 16:58:58 -0400

    Changed the name of the world file and set the default world being launched to robosub_2024

--- src/subjugator/subjugator_bringup/launch/gazebo.launch.py
+++ src/subjugator/subjugator_bringup/launch/gazebo.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
         launch_arguments={
             "gz_args": [
                 PathJoinSubstitution(
-                    [pkg_project_gazebo, "worlds", "redwhitepole.world"],  # CHANGE THIS
+                    [pkg_project_gazebo, "worlds", "robosub_2024.world"],  # CHANGE THIS
                 ),
                 " --render-engine",
                 " ogre",
