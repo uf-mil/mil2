6f8a429fae644a6e616001833ffd2db6da4cf8c6
Zhongzheng R. Zhang <zhangrenzhongzheng@outlook.com>
2025-06-11 14:17:39 -0400

    documentation page is renamed as about.  move introduction to the about page

--- src/mil_common/mil_preflight/src/uis/ftxui/ui.cpp
+++ src/mil_common/mil_preflight/src/uis/ftxui/ui.cpp
@@ -3,6 +3,7 @@
 #include <boost/dll/alias.hpp>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
 
 #include "mil_preflight/uis/ftxui/reportsPage.h"
 #include "mil_preflight/uis/ftxui/testsPage.h"
@@ -68,24 +69,25 @@ class FTXUI : public UIBase
 
         menu->Add(Button("Run Tests", [=] { currentPage_ = 1; }, menuOption));
         menu->Add(Button("View Reports", [=] { currentPage_ = 2; }, menuOption));
-        menu->Add(Button("Read Documentation", [=] { currentPage_ = 3; }, menuOption));
+        menu->Add(Button("About", [=] { currentPage_ = 3; }, menuOption));
         menu->Add(Button("Quit", [&] { screen.Exit(); }, menuOption));
 
-        Component mainPage = Renderer(menu,
-                                      [=]
-                                      {
-                                          return vbox({
-                                                     paragraph(introduction_),
-                                                     separator(),
-                                                     menu->Render() | center | flex | frame,
-                                                 }) |
-                                                 flex;
-                                      });
+        Component mainPage = menu | center | flex | frame;
 
         Component testsPage = std::make_shared<mil_preflight::TestsPage>(fileName) | flex;
         Component reportsPage = std::make_shared<mil_preflight::ReportsPage>() | flex;
 
-        Component docPage = Renderer([] { return vbox({ text("Coming soon") | center }) | flex; });
+        Component docPage = Renderer(
+            [this]
+            {
+                return vbox({ text("MIL Preflight") | bold, text("v0.1.3"), text("University of Florida"),
+                              hbox({ text("Machine Intelligence Laboratory") | hyperlink("https://mil.ufl.edu/") }),
+                              hbox({ text("Powered by "), text("FTXUI") | hyperlink("https://github.com/ArthurSonzogni/"
+                                                                                    "FTXUI/") }),
+                              separatorEmpty(), paragraph(introduction_[0]), paragraph(introduction_[1]),
+                              paragraph(introduction_[2]) }) |
+                       flex;
+            });
 
         Component body = Container::Tab({ mainPage, testsPage, reportsPage, docPage }, &currentPage_);
 
@@ -113,15 +115,13 @@ class FTXUI : public UIBase
     bool showBackButton_ = false;
     Component root_;
 
-    std::string const introduction_ = "Welcome to the Preflight Program, "
-                                      "a tool inspired by the preflight checklists used by pilots before flying a "
-                                      "plane. "
-                                      "This program is designed to verify the functionality of all software and "
-                                      "hardware "
-                                      "systems on your autonomous robot. "
-                                      "It ensures that everything is in working order, "
-                                      "allowing you to safely deploy your robot with confidence.";
-    std::vector<std::string> titles_ = { "MIL Preflight", "Tests", "Reports", "Documentation" };
+    std::string const introduction_[3] = { "MIL Preflight is a tool inspired by the preflight checklists used by "
+                                           "pilots before flying a plane.",
+                                           "This program is designed to verify the functionality of all software and "
+                                           "hardware systems on your autonomous robot.",
+                                           "It ensures that everything is in working order, allowing you to safely "
+                                           "deploy your robot with confidence." };
+    std::vector<std::string> titles_ = { "MIL Preflight", "Tests", "Reports", "About" };
 };
 
 }  // namespace mil_preflight
