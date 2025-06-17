5c847f0fff4641a80fbe4ab11389eaf3d6d8bbe9
CarterKreis <cckreis0507@gmail.com>
2025-06-17 16:16:35 -0400

    Setup Torpedos Plugin

---
+++ src/subjugator/simulation/subjugator_gazebo/include/Torpedos.hh
@@ -0,0 +1,51 @@
+#ifndef TORPEDOES__HH_
+#define TORPEDOES__HH_
+
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <gz/math/Pose3.hh>  // For gz::math::Pose3d
+#include <gz/sim/EntityComponentManager.hh>
+#include <gz/sim/InstallationDirectories.hh>
+#include <gz/sim/Model.hh>
+#include <gz/sim/System.hh>  // For gz::sim::System
+#include <gz/sim/World.hh>
+#include <gz/sim/components/Name.hh>  // For gz::sim::components::Name
+#include <gz/sim/components/Pose.hh>  // For gz::sim::components::Pose
+#include <sdf/Element.hh>
+#include <sdf/World.hh>
+#include <sdf/sdf.hh>
+
+// The .msg file is named ProcessedPing.msg (PascalCase) but #include must be the name in snake_case
+// Insert ROS Message Here if needed
+
+namespace torpedoes
+{
+class Torpedoes : public gz::sim::System, public gz::sim::ISystemConfigure, public gz::sim::ISystemPostUpdate
+{
+  public:
+    Torpedoes();
+    ~Torpedoes();
+
+    // Configure() - Gathers Info at the start of the simulation //
+    void Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
+                   gz::sim::EntityComponentManager &ecm, gz::sim::EventManager &eventMgr) override;
+
+    // System PostUpdate - Called every simulation step to update pinger/torpedoes distances //
+    void PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityComponentManager const &ecm) override;
+
+  private:
+    // Stored Entities //
+    gz::sim::Entity modelEntity_{ gz::sim::kNullEntity };      // World Entity
+    gz::sim::Entity torpedoesEntity_{ gz::sim::kNullEntity };  // Torpedoes Entity
+
+    // World SDF //
+    std::shared_ptr<sdf::Element const> sdf_;
+};
+
+}  // namespace torpedoes
+#endif  // TORPEDOES_HH
---
+++ src/subjugator/simulation/subjugator_gazebo/src/Torpedos.cc
@@ -0,0 +1,45 @@
+#include "Torpedoes.hh"
+#include "gz/plugin/Register.hh"  // For GZ_ADD_PLUGIN
+
+#include <gz/common/Console.hh>
+
+namespace torpedoes
+{
+
+Torpedoes::Torpedoes()
+{
+}
+
+Torpedoes::~Torpedoes()
+{
+}
+
+void Torpedoes::Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
+                          gz::sim::EntityComponentManager &ecm, gz::sim::EventManager &eventMgr)
+{
+    // Gather necessary information for future GatherWorldInfo() call
+    this->modelEntity_ = entity;
+
+    // Create copy of the SDF element to perform modifications due to const
+    this->sdf_ = sdf->Clone();
+
+    // Initialize the ROS node and publisher
+    if (!rclcpp::ok())
+    {
+        rclcpp::init(0, nullptr);
+    }
+}
+
+void Torpedoes::PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityComponentManager const &ecm)
+{
+    //  Check if the simulation is paused or running
+    if (info.paused)
+    {
+        return;
+    }
+}
+
+}  // namespace torpedoes
+
+// Register plugin for Gazebo
+GZ_ADD_PLUGIN(torpedoes::Torpedoes, gz::sim::System, gz::sim::ISystemConfigure, gz::sim::ISystemPostUpdate)
