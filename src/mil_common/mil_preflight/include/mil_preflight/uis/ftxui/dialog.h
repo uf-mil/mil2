8a254de5a3c93d9cc896f6eb02e7ed04776f7888
Zhongzheng R. Zhang <zhangrenzhongzheng@outlook.com>
2025-06-17 21:03:29 -0400

    add ability to select which config file to load

--- src/mil_common/mil_preflight/include/mil_preflight/uis/ftxui/dialog.h
+++ src/mil_common/mil_preflight/include/mil_preflight/uis/ftxui/dialog.h
@@ -11,87 +11,107 @@ namespace mil_preflight
 {
 using namespace ftxui;
 
-class Dialog : public ComponentBase, public std::enable_shared_from_this<Dialog>
+class Window : public ComponentBase, public std::enable_shared_from_this<Window>
 {
   public:
     struct Option
     {
         std::string title;
-        std::string question;
-        std::vector<std::string> buttonLabels;
     };
 
-    Dialog(Option const& option) : option_(option), screen_(ScreenInteractive::Fullscreen())
+    Window(std::string const& title) : screen_(ScreenInteractive::Fullscreen())
     {
-        create();
+        Component close_button = Button("X", [=] { screen_.Exit(); }, ButtonOption::Ascii());
+
+        head_ = Container::Horizontal({ Renderer([=] { return text(title); }) | center | flex, close_button });
     }
 
-    Dialog(Option&& option) : option_(std::move(option)), screen_(ScreenInteractive::Fullscreen())
+    ~Window()
     {
-        create();
+    }
+
+    void show(Component content)
+    {
+        Component main = Container::Vertical({ head_, Renderer([] { return separator(); }), content | flex });
+        Add(main | border);
+
+        screen_.Loop(shared_from_this());
+    }
+
+    void close()
+    {
+        screen_.Exit();
+    }
+
+  protected:
+  private:
+    Component head_;
+    ScreenInteractive screen_;
+};
+
+class Dialog : public Window
+{
+  public:
+    Dialog(std::string const& title) : Window(title)
+    {
     }
 
     ~Dialog()
     {
     }
 
-    int show()
+    int show(Component content, std::vector<std::string> const& options)
     {
-        screen_.Loop(shared_from_this());
-        return index_;
-    }
-
-    Element Render() override
-    {
-        return vbox({ hbox({ text(option_.title) | flex, closeButton_->Render() }), separator(),
-                      vbox(paragraphs_) | flex, separator(), buttonsContainer_->Render() | center }) |
-               border;
-    }
-
-  private:
-    Component buttonsContainer_;
-    Component closeButton_;
-    Elements paragraphs_;
-    Option option_;
-    ScreenInteractive screen_;
-    int index_ = -1;
-
-    void create()
-    {
-        std::istringstream iss(option_.question);
-        std::string line;
-        while (std::getline(iss, line))
-        {
-            paragraphs_.push_back(paragraph(line));
-        }
-
+        int ret = -1;
         Components buttons;
-        for (size_t i = 0; i < option_.buttonLabels.size(); i++)
+        for (size_t i = 0; i < options.size(); i++)
         {
             buttons.push_back(Button(
-                option_.buttonLabels[i],
-                [=]
+                options[i],
+                [this, &ret, i]
                 {
-                    index_ = i;
-                    screen_.Exit();
+                    ret = i;
+                    close();
                 },
                 ButtonOption::Border()));
         }
-        buttonsContainer_ = Container::Horizontal(buttons);
 
-        closeButton_ = Button(
-            "X",
-            [=]
-            {
-                index_ = -1;
-                screen_.Exit();
-            },
-            ButtonOption::Ascii());
+        Component main = Container::Vertical(
+            { content | flex, Renderer([] { return separator(); }), Container::Horizontal(buttons) | center });
 
-        Component dialogContainer = Container::Vertical({ closeButton_, buttonsContainer_ });
-
-        Add(dialogContainer);
+        Window::show(main);
+        return ret;
     }
+
+  private:
+};
+
+class MessageBox : public Dialog
+{
+  public:
+    MessageBox(std::string const& title) : Dialog(title)
+    {
+    }
+
+    ~MessageBox()
+    {
+    }
+
+    int show(std::string const& message, std::vector<std::string> const& options)
+    {
+        std::istringstream iss(message);
+        std::string line;
+        Elements paragraphs;
+        while (std::getline(iss, line))
+        {
+            paragraphs.push_back(paragraph(line));
+        }
+
+        return Dialog::show(Renderer([&] { return vbox({ paragraphs }); }), options);
+    }
+
+  private:
+    Option option_;
 };
 
 }  // namespace mil_preflight
