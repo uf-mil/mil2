2e10fd6aa1385b236ad731bebcac49ca5c60b6b4
CarterKreis <cckreis0507@gmail.com>
2025-06-24 17:22:49 -0400

    World name gathered

--- src/subjugator/simulation/subjugator_gazebo/include/Torpedoes.hh
+++ src/subjugator/simulation/subjugator_gazebo/include/Torpedoes.hh
@@ -1,13 +1,18 @@
 #ifndef TORPEDOES__HH_
 #define TORPEDOES__HH_
 
+#include <gz/msgs/entity_factory.pb.h>
+
 #include <chrono>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
 #include <rclcpp/rclcpp.hpp>
 
+#include <ament_index_cpp/get_package_share_directory.hpp>
 #include <gz/math/Pose3.hh>  // For gz::math::Pose3d
 #include <gz/sim/EntityComponentManager.hh>
 #include <gz/sim/EventManager.hh>
@@ -15,8 +20,12 @@
 #include <gz/sim/Model.hh>
 #include <gz/sim/System.hh>  // For gz::sim::System
 #include <gz/sim/World.hh>
+#include <gz/sim/components/AngularVelocity.hh>
+#include <gz/sim/components/LinearVelocity.hh>
 #include <gz/sim/components/Name.hh>  // For gz::sim::components::Name
 #include <gz/sim/components/Pose.hh>  // For gz::sim::components::Pose
+#include <gz/sim/components/World.hh>
+#include <gz/transport/Node.hh>
 #include <sdf/Element.hh>
 #include <sdf/World.hh>
 #include <sdf/sdf.hh>
@@ -36,16 +45,30 @@ class Torpedoes : public gz::sim::System, public gz::sim::ISystemConfigure, publ
     void Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
                    gz::sim::EntityComponentManager &ecm, gz::sim::EventManager &eventMgr) override;
 
+    // SpawnTorpedo - Spawns a torpedo in the simulation //
+    void SpawnTorpedo(std::string const &worldName, std::string const &sdfPath);
+
     // System PostUpdate - Called every simulation step to update pinger/torpedoes distances //
     void PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityComponentManager const &ecm) override;
 
   private:
-    // Stored Entities //
-    gz::sim::Entity modelEntity_{ gz::sim::kNullEntity };      // World Entity
-    gz::sim::Entity torpedoesEntity_{ gz::sim::kNullEntity };  // Torpedoes Entity
-
-    // Sub9 SDF //
+    // Sub9 SDF & Entity //
     std::shared_ptr<sdf::Element const> sub9_SDF;
+    gz::sim::Entity sub9_Entity{ gz::sim::kNullEntity };
+
+    // Torpedo Variables //
+    int torpedoCount = 0;
+    bool worldNameFound = false;
+    std::string worldName = "robosub_2024.world";
+
+    // Torpedo SDF Filepath //
+    std::string const Torpedo_sdfPath = ament_index_cpp::get_package_share_directory("subjugator_description") + "/mode"
+                                                                                                                 "ls/"
+                                                                                                                 "torpe"
+                                                                                                                 "do/"
+                                                                                                                 "torpe"
+                                                                                                                 "do."
+                                                                                                                 "sdf";
 };
 
 }  // namespace torpedoes
--- src/subjugator/simulation/subjugator_gazebo/src/Torpedoes.cc
+++ src/subjugator/simulation/subjugator_gazebo/src/Torpedoes.cc
@@ -19,15 +19,42 @@ void Torpedoes::Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::El
                           gz::sim::EntityComponentManager &ecm, gz::sim::EventManager &eventMgr)
 {
     // Gather necessary information for future GatherWorldInfo() call
-    this->modelEntity_ = entity;
+    this->sub9_Entity = entity;
 
     // Create copy of the Sub SDF element to perform modifications due to const
     this->sub9_SDF = sdf->Clone();
+
+    // Get the Torpedo SDF & Entity from ECM
 
     // Initialize the ROS node and publisher
     if (!rclcpp::ok())
     {
         rclcpp::init(0, nullptr);
+    }
+}
+
+void Torpedoes::SpawnTorpedo(std::string const &worldName, std::string const &sdfPath)
+{
+    // Load SDF file as string
+    std::ifstream sdfFile(sdfPath);
+    std::stringstream buffer;
+    buffer << sdfFile.rdbuf();
+    std::string sdfString = buffer.str();
+
+    // Prepare the factory message
+    gz::msgs::EntityFactory factoryMsg;
+    factoryMsg.set_sdf(sdfString);
+
+    // Optionally, set the pose if you want to spawn at a specific location
+    // gz::msgs::Set(factoryMsg.mutable_pose(), gz::math::Pose3d(x, y, z, roll, pitch, yaw));
+
+    // Send the request
+    gz::transport::Node node;
+    std::string service = "/world/" + worldName + "/create";
+    bool result = node.Request(service, factoryMsg, 1000);
+    if (!result)
+    {
+        gzerr << "Failed to spawn torpedo model." << std::endl;
     }
 }
 
@@ -37,6 +64,27 @@ void Torpedoes::PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::EntityCompo
     if (info.paused)
     {
         return;
+    }
+
+    if (!worldNameFound)
+    {
+        ecm.Each<gz::sim::components::World, gz::sim::components::Name>(
+            [&](gz::sim::Entity const &entity, gz::sim::components::World const *,
+                gz::sim::components::Name const *name) -> bool
+            {
+                this->worldName = name->Data();
+                std::cout << "[Torpedoes] World Name Found: " << worldName << std::endl;
+                worldNameFound = true;
+                return false;  // Stop after finding the first world
+            });
+    }
+
+    // Spawn two torpedoes maximum
+    if (torpedoCount < 2)
+    {
+        std::cout << "[Torpedoes] Torpedo Count: " << torpedoCount << std::endl;
+        // this->SpawnTorpedo(this->worldName, sdfPath);
+        torpedoCount++;
     }
 }
 
