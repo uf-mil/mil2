06960e683fe3bf86ac0fe239269ed6761406dd8b
Zhongzheng R. Zhang <zhangrenzhongzheng@outlook.com>
2025-07-01 10:28:46 -0400

    add a Frontend class, move job handle logic to it

--- src/mil_common/mil_preflight/include/mil_preflight/ui.h
+++ src/mil_common/mil_preflight/include/mil_preflight/ui.h
@@ -15,11 +15,11 @@
 namespace mil_preflight
 {
 
-class UIBase;
+class Frontend;
 class Action
 {
   public:
-    friend class UIBase;
+    friend class Frontend;
 
     Action() {};
     ~Action() {};
@@ -30,6 +30,8 @@ class Action
   protected:
     virtual void onStart() = 0;
     virtual void onFinish(bool success, std::string&& summery) = 0;
+    virtual std::shared_future<int> onQuestion(std::string&& question, std::vector<std::string>&& options) = 0;
+
     std::vector<std::string> stdouts;
     std::vector<std::string> stderrs;
 
@@ -39,7 +41,7 @@ class Action
 class Test
 {
   public:
-    friend class UIBase;
+    friend class Frontend;
 
     Test() {};
     ~Test() {};
@@ -55,7 +57,7 @@ class Test
 class Job
 {
   public:
-    friend class UIBase;
+    friend class Frontend;
 
     Job() {};
     ~Job() {};
@@ -65,228 +67,52 @@ class Job
     virtual void onFinish() = 0;
 };
 
+class Frontend
+{
+  public:
+    Frontend(int argc, char* argv[]);
+    ~Frontend();
+
+    void runJob(Job& job);
+    void runJobAsync(Job& job);
+    std::vector<std::string> const& getArgs()
+    {
+        return args_;
+    }
+
+  private:
+    std::vector<std::string> args_;
+
+    boost::filesystem::path bin_path_ = boost::process::search_path("mil_preflight_backend");
+
+    boost::asio::io_context work_context_;
+    boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard_;
+    std::thread work_thread_;
+
+    boost::process::ipstream child_out_;
+    boost::process::ipstream child_err_;
+    boost::process::opstream child_in_;
+
+    boost::process::child backend_;
+
+    void runTest(Test& test);
+    void runAction(Action& action);
+};
+
 class UIBase
 {
   public:
     UIBase()
-      : work_guard(boost::asio::make_work_guard(work_context))
-      , work_thread([this] { work_context.run(); })
-      , backend(boost::process::child(bin_path.string(),
-                                      boost::process::std_in<child_in, boost::process::std_out> child_out,
-                                      boost::process::std_err > child_err))
     {
     }
-
     virtual ~UIBase()
     {
-        work_guard.reset();
-        work_context.stop();
-        work_thread.join();
-
-        if (backend.running())
-            child_in << EOT << std::endl;
-
-        backend.join();
     }
 
-    virtual int spin()
+    virtual int run([[maybe_unused]] std::shared_ptr<Frontend> frontend)
     {
-        return -1;
-    }
-
-    virtual bool initialize([[maybe_unused]] int argc, char* argv[])
-    {
-        std::cout << "Unrecognized ui type " << argv[0] << std::endl;
-        return false;
-    }
-
-    void runJob(Job& job)
-    {
-        std::optional<std::reference_wrapper<Test>> testOptional = job.nextTest();
-
-        while (testOptional.has_value())
-        {
-            Test& test = testOptional.value();
-            runTest(test);
-
-            testOptional = job.nextTest();
-        }
-
-        job.onFinish();
-    }
-
-    void runJobAsync(Job& job)
-    {
-        work_context.post([&] { runJob(job); });
-    }
-
-  protected:
-    virtual std::shared_future<int> onQuestion([[maybe_unused]] std::string&& question,
-                                               [[maybe_unused]] std::vector<std::string>&& options)
-    {
-        std::promise<int> question_promise;
-        question_promise.set_value(-1);
-        return question_promise.get_future().share();
-    }
-
-  private:
-    boost::filesystem::path bin_path = boost::process::search_path("mil_preflight_backend");
-
-    boost::asio::io_context work_context;
-    boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard;
-    std::thread work_thread;
-
-    boost::process::ipstream child_out;
-    boost::process::ipstream child_err;
-    boost::process::opstream child_in;
-
-    boost::process::child backend;
-
-    void runTest(Test& test)
-    {
-        std::optional<std::reference_wrapper<Action>> actionOptional = test.nextAction();
-
-        child_in << test.getPlugin() << std::endl;
-
-        while (actionOptional.has_value())
-        {
-            Action& action = actionOptional.value();
-            runAction(action);
-
-            actionOptional = test.nextAction();
-        }
-
-        if (!child_in.fail())
-            child_in << EOT << std::endl;
-
-        test.onFinish();
-    }
-
-    void runAction(Action& action)
-    {
-        action.onStart();
-
-        enum class State
-        {
-            START,
-            STDOUT,
-            SUMMERY,
-            QUESTION,
-            OPTIONS,
-        } state = State::START;
-
-        std::string line;
-        std::string summery;
-        bool success = false;
-        action.stdouts.clear();
-        action.stderrs.clear();
-
-        std::string question;
-        std::vector<std::string> options;
-
-        while (true)
-        {
-            if (state == State::START)
-            {
-                try
-                {
-                    child_in << action.getName() << std::endl;
-                    for (std::string const& parameter : action.getParameters())
-                    {
-                        child_in << parameter << std::endl;
-                    }
-                    child_in << GS << std::endl;
-                }
-                catch (std::exception const& e)
-                {
-                    summery = "Broken pipe: " + std::string(e.what());
-                    break;
-                }
-                state = State::STDOUT;
-            }
-            else if (state == State::STDOUT)
-            {
-                if (!std::getline(child_out, line))
-                {
-                    summery = "Broken pipe";
-                    break;
-                }
-
-                if (line[0] == ACK)
-                {
-                    state = State::SUMMERY;
-                    success = true;
-                }
-                else if (line[0] == NCK)
-                {
-                    state = State::SUMMERY;
-                }
-                else if (line[0] == BEL)
-                {
-                    state = State::QUESTION;
-                }
-                else
-                {
-                    action.stdouts.push_back(std::move(line));
-                }
-            }
-            else if (state == State::QUESTION)
-            {
-                if (!std::getline(child_out, question, GS))
-                {
-                    summery = "Broken pipe";
-                    break;
-                }
-
-                state = State::OPTIONS;
-            }
-            else if (state == State::OPTIONS)
-            {
-                if (!std::getline(child_out, line, GS))
-                {
-                    summery = "Broken pipe";
-                    break;
-                }
-
-                if (line[0] == EOT)
-                {
-                    std::shared_future<int> feedback = onQuestion(std::move(question), std::move(options));
-                    try
-                    {
-                        child_in << feedback.get() << std::endl;
-                        options.clear();
-                    }
-                    catch (std::exception const& e)
-                    {
-                        summery = "Broken pipe: " + std::string(e.what());
-                        break;
-                    }
-                    state = State::STDOUT;
-                }
-                else
-                {
-                    options.push_back(std::move(line));
-                }
-            }
-            else if (state == State::SUMMERY)
-            {
-                if (!std::getline(child_out, summery))
-                {
-                    break;
-                }
-
-                while (std::getline(child_err, line))
-                {
-                    if (line[0] == EOT)
-                        break;
-                    action.stderrs.push_back(std::move(line));
-                }
-
-                break;
-            }
-        }
-
-        action.onFinish(success, std::move(summery));
+        std::cout << "Unrecognized ui type " << frontend->getArgs()[0] << std::endl;
+        return 1;
     }
 };
 
--- src/mil_common/mil_preflight/src/frontend.cpp
+++ src/mil_common/mil_preflight/src/frontend.cpp
@@ -3,36 +3,199 @@
 
 #include "mil_preflight/ui.h"
 
-using Creator = std::shared_ptr<mil_preflight::UIBase>();
-std::function<Creator> load(std::string const& uiName)
+namespace mil_preflight
 {
-    try
+
+Frontend::Frontend(int argc, char* argv[])
+  : work_guard_(boost::asio::make_work_guard(work_context_))
+  , work_thread_([this] { work_context_.run(); })
+  , backend_(boost::process::child(bin_path_.string(),
+                                   boost::process::std_in<child_in_, boost::process::std_out> child_out_,
+                                   boost::process::std_err > child_err_))
+{
+    for (int i = 0; i < argc; i++)
     {
-        return boost::dll::import_alias<Creator>(
-            uiName, uiName, boost::dll::load_mode::append_decorations | boost::dll::load_mode::search_system_folders);
-    }
-    catch (boost::system::system_error const& e)
-    {
-        return []() -> std::shared_ptr<mil_preflight::UIBase> { return std::make_shared<mil_preflight::UIBase>(); };
+        args_.emplace_back(argv[i]);
     }
 }
 
-int main(int argc, char* argv[])
+Frontend::~Frontend()
 {
-    if (argc <= 1)
-    {
-        std::cout << "Usage: mil_preflight <ui> [ui_parameters...]" << std::endl;
-        return -1;
-    }
+    work_guard_.reset();
+    work_context_.stop();
+    work_thread_.join();
 
-    std::function<Creator> creator = load(argv[1]);
-    std::shared_ptr<mil_preflight::UIBase> ui = creator();
+    if (backend_.running())
+        child_in_ << EOT << std::endl;
 
-    if (!ui->initialize(argc - 1, &argv[1]))
-    {
-        std::cout << "Failed to initialize the ui" << std::endl;
-        return 1;
-    }
-
-    return ui->spin();
+    backend_.join();
 }
+
+void Frontend::runJob(Job& job)
+{
+    std::optional<std::reference_wrapper<Test>> testOptional = job.nextTest();
+
+    while (testOptional.has_value())
+    {
+        Test& test = testOptional.value();
+        runTest(test);
+
+        testOptional = job.nextTest();
+    }
+
+    job.onFinish();
+}
+
+void Frontend::runJobAsync(Job& job)
+{
+    work_context_.post([&] { runJob(job); });
+}
+
+void Frontend::runTest(Test& test)
+{
+    std::optional<std::reference_wrapper<Action>> actionOptional = test.nextAction();
+
+    child_in_ << test.getPlugin() << std::endl;
+
+    while (actionOptional.has_value())
+    {
+        Action& action = actionOptional.value();
+        runAction(action);
+
+        actionOptional = test.nextAction();
+    }
+
+    if (!child_in_.fail())
+        child_in_ << EOT << std::endl;
+
+    test.onFinish();
+}
+
+void Frontend::runAction(Action& action)
+{
+    action.onStart();
+
+    enum class State
+    {
+        START,
+        STDOUT,
+        SUMMERY,
+        QUESTION,
+        OPTIONS,
+    } state = State::START;
+
+    std::string line;
+    std::string summery;
+    bool success = false;
+    action.stdouts.clear();
+    action.stderrs.clear();
+
+    std::string question;
+    std::vector<std::string> options;
+
+    while (true)
+    {
+        if (state == State::START)
+        {
+            try
+            {
+                child_in_ << action.getName() << std::endl;
+                for (std::string const& parameter : action.getParameters())
+                {
+                    child_in_ << parameter << std::endl;
+                }
+                child_in_ << GS << std::endl;
+            }
+            catch (std::exception const& e)
+            {
+                summery = "Broken pipe: " + std::string(e.what());
+                break;
+            }
+            state = State::STDOUT;
+        }
+        else if (state == State::STDOUT)
+        {
+            if (!std::getline(child_out_, line))
+            {
+                summery = "Broken pipe";
+                break;
+            }
+
+            if (line[0] == ACK)
+            {
+                state = State::SUMMERY;
+                success = true;
+            }
+            else if (line[0] == NCK)
+            {
+                state = State::SUMMERY;
+            }
+            else if (line[0] == BEL)
+            {
+                state = State::QUESTION;
+            }
+            else
+            {
+                action.stdouts.push_back(std::move(line));
+            }
+        }
+        else if (state == State::QUESTION)
+        {
+            if (!std::getline(child_out_, question, GS))
+            {
+                summery = "Broken pipe";
+                break;
+            }
+
+            state = State::OPTIONS;
+        }
+        else if (state == State::OPTIONS)
+        {
+            if (!std::getline(child_out_, line, GS))
+            {
+                summery = "Broken pipe";
+                break;
+            }
+
+            if (line[0] == EOT)
+            {
+                std::shared_future<int> feedback = action.onQuestion(std::move(question), std::move(options));
+                try
+                {
+                    child_in_ << feedback.get() << std::endl;
+                    options.clear();
+                }
+                catch (std::exception const& e)
+                {
+                    summery = "Broken pipe: " + std::string(e.what());
+                    break;
+                }
+                state = State::STDOUT;
+            }
+            else
+            {
+                options.push_back(std::move(line));
+            }
+        }
+        else if (state == State::SUMMERY)
+        {
+            if (!std::getline(child_out_, summery))
+            {
+                break;
+            }
+
+            while (std::getline(child_err_, line))
+            {
+                if (line[0] == EOT)
+                    break;
+                action.stderrs.push_back(std::move(line));
+            }
+
+            break;
+        }
+    }
+
+    action.onFinish(success, std::move(summery));
+}
+
+}  // namespace mil_preflight
