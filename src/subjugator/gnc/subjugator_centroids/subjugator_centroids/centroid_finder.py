a74e2e143aa88b83f56ab2c353f979d84c9edb59
Joseph Handsome <jgoodman1@ufl.edu>
2025-07-17 18:36:11 -0400

    renaming stuff

--- src/subjugator/gnc/subjugator_centroids/subjugator_centroids/centroid_finder.py
+++ src/subjugator/gnc/subjugator_centroids/subjugator_centroids/centroid_finder.py
@@ -1,18 +1,19 @@
 from abc import ABC, abstractmethod
+
 from cv2.typing import MatLike
 
 """
 If you want to track some object, you need a class that implements this class
 """
-class CentriodFinder(ABC):
-    # should be self explanitory
+
+
+class CentroidFinder(ABC):
     @property
     @abstractmethod
     def topic_name(self) -> str:
         pass
 
-    # returns (x, y) pair of centriod or None if not found
+    # returns (x, y) pair of centroid or None if not found
     @abstractmethod
-    def find_centriod(self, frame: MatLike) -> tuple[int, int] | None:
+    def find_centroid(self, frame: MatLike) -> tuple[int, int] | None:
         pass
-
