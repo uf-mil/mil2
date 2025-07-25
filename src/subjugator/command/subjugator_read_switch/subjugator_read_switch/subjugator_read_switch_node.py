793200b70abf4adfbcf664ffde940af9985bc69c
Joseph Handsome <nottellingu@gmail.com>
2025-07-25 10:40:15 -0400

    read swtich can now bypass the kill system
    what have i done
    i am become death

--- src/subjugator/command/subjugator_read_switch/subjugator_read_switch/subjugator_read_switch_node.py
+++ src/subjugator/command/subjugator_read_switch/subjugator_read_switch/subjugator_read_switch_node.py
@@ -2,6 +2,7 @@ import Jetson.GPIO as GPIO
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from std_srvs.srv import Empty, SetBool
 
 INPUT_PIN = 18
 
@@ -14,6 +15,24 @@ class SubjugatorReadSwitchNode(Node):
         self.pub = self.create_publisher(String, "read_switch", 10)
         self.prev_value = False  # false = low, true = high
 
+        # reset localization
+        self.localization_client = self.create_client(
+            Empty,
+            "/subjugator_localization/reset",
+        )
+
+        # reset controller
+        self.controller_reset_client = self.create_client(
+            Empty,
+            "/pid_controller/reset",
+        )
+
+        # start vs stop controller
+        self.controller_client = self.create_client(SetBool, "/pid_controller/enable")
+
+        # unkill
+        self.unkill_client = self.create_client(Empty, "/unkill")
+
     def check_gpio(self):
         value = GPIO.input(INPUT_PIN)
         msg_str = "high" if value == GPIO.HIGH else "low"
@@ -25,7 +44,29 @@ class SubjugatorReadSwitchNode(Node):
             self.reset_sub()
 
     def reset_sub(self):
-        pass
+        msg = Empty().Request()
+
+        start_msg = SetBool().Request()
+        start_msg.data = True
+
+        stop_msg = SetBool().Request()
+        stop_msg.data = False
+
+        # stop-controller
+        self.controller_client.call(stop_msg)
+        rclpy.spin_once(self, timeout_sec=0.5)
+
+        # unkill
+        self.unkill_client.call(msg)
+
+        # reset-localization
+        self.localization_client.call(msg)
+
+        # reset-controller
+        self.controller_reset_client.call(msg)
+
+        # start-controller
+        self.controller_client.call(start_msg)
 
     def __del__(self):
         GPIO.cleanup()
