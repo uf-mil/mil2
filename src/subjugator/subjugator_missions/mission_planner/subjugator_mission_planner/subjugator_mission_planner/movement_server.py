08c6416d2970d60c9eec0116bf7b6a7380e1034a
Adam McAleer <amcaleer1127@gmail.com>
2025-07-28 14:26:54 -0400

    implemented use of mission support in movement server

--- src/subjugator/subjugator_missions/mission_planner/subjugator_mission_planner/subjugator_mission_planner/movement_server.py
+++ src/subjugator/subjugator_missions/mission_planner/subjugator_mission_planner/subjugator_mission_planner/movement_server.py
@@ -1,4 +1,3 @@
-import math
 import time
 
 import mission_support
@@ -55,20 +54,6 @@ class MovementServer(Node):
     # Gets current pose
     def odom_callback(self, msg):
         self.current_pose = msg.pose.pose
-
-    def check_at_goal_pose(self, currentPose, goalPose, acceptableDist=0.05):
-        x_dist = currentPose.position.x - goalPose.position.x
-        y_dist = currentPose.position.y - goalPose.position.y
-        z_dist = currentPose.position.z - goalPose.position.z
-
-        i_dist = currentPose.orientation.x - goalPose.orientation.x
-        j_dist = currentPose.orientation.y - goalPose.orientation.y
-        k_dist = currentPose.orientation.z - goalPose.orientation.z
-        w_dist = currentPose.orientation.w - goalPose.orientation.w
-
-        distance_to_goal = math.sqrt(x_dist**2 + y_dist**2 + z_dist**2)
-        orientation_to_goal = math.sqrt(i_dist**2 + j_dist**2 + k_dist**2 + w_dist**2)
-        return distance_to_goal < acceptableDist and orientation_to_goal < 0.1
 
     def execute_callback(self, goal_handle):
         self.get_logger().info(
