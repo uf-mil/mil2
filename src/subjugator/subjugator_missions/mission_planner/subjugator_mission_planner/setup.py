41046ec43e4e4bcecf22f3ca35a9fe202c8f34a7
TheDinner22 <79279956+TheDinner22@users.noreply.github.com>
2025-08-11 21:14:24 -0800

    Joe simple sonar (#219)
    * adds goto sonar code to mission planner
    
    better code would triangulate and rotate, this is just proof of concept for tmr
    
    * fix typo and move multiple times
    
    * adds sonar follower to setup
    
    * surfaces after passing sonar
    
    * fixes typo
    
    ---------
    
    Co-authored-by: Joseph Handsome <nottellingu@gmail.com>
    Co-authored-by: Joseph Handsome <jgoodman1@ufl.edu>

--- src/subjugator/subjugator_missions/mission_planner/subjugator_mission_planner/setup.py
+++ src/subjugator/subjugator_missions/mission_planner/subjugator_mission_planner/setup.py
@@ -41,6 +41,7 @@ setup(
             "start_gate_server = subjugator_mission_planner.start_gate_server:main",
             "yawtracker = subjugator_mission_planner.yaw_tracker_server:main",
             "mechanisms_server = subjugator_mission_planner.mechanisms_server:main",
+            "sonar_follower = subjugator_mission_planner.sonar_follower_server:main",
             "nav_channel_server = subjugator_mission_planner.nav_channel_server:main",
         ],
     },
