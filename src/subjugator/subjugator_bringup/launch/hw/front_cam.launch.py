da4e75cafe190ee5c0bdc1c04f9a58ae1d38d8a7
Joseph Handsome <jgoodman1@ufl.edu>
2025-10-13 13:52:39 -0400

    adds frontcam to hw launch

--- src/subjugator/subjugator_bringup/launch/hw/front_cam.launch.py
+++ src/subjugator/subjugator_bringup/launch/hw/front_cam.launch.py
@@ -2,22 +2,19 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            Node(
-                package="front_cam",
-                executable="front_cam",
-                name="front_cam",
-                parameters=[
-                    os.path.join(
-                        get_package_share_directory("front_cam"),
-                    ),
-                ],
-                output="screen",
-            ),
-        ],
+    front_cam_package = get_package_share_directory("front_cam")
+
+    front_cam_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(front_cam_package, "launch", "front_cam.launch.py"),
+        ),
     )
+
+    ld = LaunchDescription()
+    ld.add_action(front_cam_launch)
+    return ld
