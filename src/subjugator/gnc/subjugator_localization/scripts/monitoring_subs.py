92a71aa1b6b4456c3e13865a53bbc8225cf9e3b2
emilyzucker <emilyzucker@ufl.edu>
2025-10-16 17:22:03 -0400

    figuring out how why spike always detected

--- src/subjugator/gnc/subjugator_localization/scripts/monitoring_subs.py
+++ src/subjugator/gnc/subjugator_localization/scripts/monitoring_subs.py
@@ -26,13 +26,13 @@ class MonitoringNode(Node):
     def imu_data_callback(self, imsg: Imu):
         # create running average for relevant measurements:
         # acceleration x
-        if self.imu_accelerationx_array.size() <= 3:
+        if len(self.imu_accelerationx_array) <= 3:
             self.imu_accelerationx_array.append(imsg.linear_acceleration.x)
         else:
             imu_accelerationx_avg = (
                 self.imu_accelerationx_array[0]
-                + self.imu_acceleration_array[1]
-                + self.imu_acceleration_array[2]
+                + self.imu_accelerationx_array[1]
+                + self.imu_accelerationx_array[2]
             ) / len(self.imu_accelerationx_array)
             percent_diff = (
                 (imsg.linear_acceleration.x - imu_accelerationx_avg)
@@ -42,10 +42,10 @@ class MonitoringNode(Node):
                 self.imu_accelerationx_array.append(imsg.linear_acceleration.x)
                 del self.imu_accelerationx_array[0]
             else:
-                self.get_logger().info("Spike Detected.")
+                self.get_logger().info("Spike Detected at lin_accel_x.")
 
         # acceleration y
-        if self.imu_accelerationy_array.size() <= 3:
+        if len(self.imu_accelerationy_array) <= 3:
             self.imu_accelerationy_array.append(imsg.linear_acceleration.y)
         else:
             imu_accelerationy_avg = (
@@ -61,10 +61,10 @@ class MonitoringNode(Node):
                 self.imu_accelerationy_array.append(imsg.linear_acceleration.y)
                 del self.imu_accelerationy_array[0]
             else:
-                self.get_logger().info("Spike Detected.")
+                self.get_logger().info("Spike Detected a lin_accel_y.")
 
         # acceleration z
-        if self.imu_accelerationz_array.size() <= 3:
+        if len(self.imu_accelerationz_array) <= 3:
             self.imu_accelerationz_array.append(imsg.linear_acceleration.z)
         else:
             imu_accelerationz_avg = (
@@ -82,7 +82,7 @@ class MonitoringNode(Node):
                 self.imu_accelerationz_array.append(imsg.linear_acceleration.z)
                 del self.imu_accelerationz_array[0]
             else:
-                self.get_logger().info("Spike Detected.")
+                self.get_logger().info("Spike Detected at lin_accel_z.")
 
         self.pub_imu_.publish(imsg)
 
