a2f6dba824daa401f9e09b5a303369401c766275
Zhongzheng R. Zhang <zhangrenzhongzheng@outlook.com>
2025-10-25 10:17:56 -0400

    remove the cppystruct lib

--- ext/cppystruct/include/cppystruct.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "cppystruct/calcsize.h"
-#include "cppystruct/data_view.h"
-#include "cppystruct/format.h"
-#include "cppystruct/pack.h"
-#include "cppystruct/string.h"
-#include "cppystruct/unpack.h"
