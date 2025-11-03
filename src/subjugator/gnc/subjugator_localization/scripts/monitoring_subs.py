41c5e737c040c884dfe266a69fd8a1e7c00325c3
emilyzucker <emilyzucker@ufl.edu>
2025-11-02 19:28:21 -0500

    cleaning up spikes

--- src/subjugator/gnc/subjugator_localization/scripts/monitoring_subs.py
+++ src/subjugator/gnc/subjugator_localization/scripts/monitoring_subs.py
@@ -20,6 +20,10 @@ class MonitoringNode(Node):
         self.imu_accelerationy_array = []
         self.imu_accelerationz_array = []
 
+        self.in_spike_statex = False
+        self.in_spike_statey = False
+        self.in_spike_statez = False
+
     def dvl_odom_callback(self, dmsg: Odometry):
         self.pub_dvl_.publish(dmsg)
 
@@ -55,13 +59,16 @@ class MonitoringNode(Node):
                 (abs(imsg.linear_acceleration.x) - imu_accelerationx_avg)
                 / imu_accelerationx_avg
             ) * 100
-            if -500 < percent_diff < 500:  # arbitrary large percentage difference
+            if -3000 < percent_diff < 3000:  # arbitrary large percentage difference
                 self.imu_accelerationx_array.append(imsg.linear_acceleration.x)
                 del self.imu_accelerationx_array[0]
+                self.in_spike_statex = False
             else:
-                self.get_logger().info(
-                    f"Spike Detected at lin_accel_x: {imsg.linear_acceleration.x}",
-                )
+                if not self.in_spike_statex:
+                    self.get_logger().info(
+                        f"Spike Detected at lin_accel_x: {imsg.linear_acceleration.x}",
+                    )
+                    self.in_spike_statex = True
 
         # acceleration y
         if len(self.imu_accelerationy_array) <= 20:
@@ -93,34 +100,57 @@ class MonitoringNode(Node):
                 (imsg.linear_acceleration.y - imu_accelerationy_avg)
                 / imu_accelerationy_avg
             ) * 100
-            if -500 < percent_diff < 500:  # arbitrary large percentage difference
+            if -3000 < percent_diff < 3000:  # arbitrary large percentage difference
                 self.imu_accelerationy_array.append(imsg.linear_acceleration.y)
                 del self.imu_accelerationy_array[0]
+                self.in_spike_statey = False
             else:
-                self.get_logger().info(
-                    f"Spike Detected a lin_accel_y: {imsg.linear_acceleration.y}",
-                )  # tells us where spike occurred.
+                if not self.in_spike_statey:
+                    self.get_logger().info(
+                        f"Spike Detected at lin_accel_y: {imsg.linear_acceleration.y}",
+                    )
+                    self.in_spike_statey = True
 
         # acceleration z
-        if len(self.imu_accelerationz_array) <= 3:
+        if len(self.imu_accelerationz_array) <= 20:
             self.imu_accelerationz_array.append(imsg.linear_acceleration.z)
         else:
             imu_accelerationz_avg = (
-                self.imu_accelerationz_array[0]
-                + self.imu_accelerationz_array[1]
-                + self.imu_accelerationz_array[2]
+                abs(self.imu_accelerationz_array[0])
+                + abs(self.imu_accelerationz_array[1])
+                + abs(self.imu_accelerationz_array[2])
+                + abs(self.imu_accelerationz_array[3])
+                + abs(self.imu_accelerationz_array[4])
+                + abs(self.imu_accelerationz_array[5])
+                + abs(self.imu_accelerationz_array[6])
+                + abs(self.imu_accelerationz_array[7])
+                + abs(self.imu_accelerationz_array[8])
+                + abs(self.imu_accelerationz_array[9])
+                + abs(self.imu_accelerationz_array[10])
+                + abs(self.imu_accelerationz_array[12])
+                + abs(self.imu_accelerationz_array[12])
+                + abs(self.imu_accelerationz_array[13])
+                + abs(self.imu_accelerationz_array[14])
+                + abs(self.imu_accelerationz_array[15])
+                + abs(self.imu_accelerationz_array[16])
+                + abs(self.imu_accelerationz_array[17])
+                + abs(self.imu_accelerationz_array[18])
+                + abs(self.imu_accelerationz_array[19])
             ) / len(self.imu_accelerationz_array)
             percent_diff = (
                 (imsg.linear_acceleration.z - imu_accelerationz_avg)
                 / imu_accelerationz_avg
             ) * 100
-            if (
-                percent_diff < 500 or percent_diff > -500
-            ):  # arbitrary large percentage difference
+            if -3000 < percent_diff < 3000:  # arbitrary large percentage difference
                 self.imu_accelerationz_array.append(imsg.linear_acceleration.z)
                 del self.imu_accelerationz_array[0]
+                self.in_spike_statez = False
             else:
-                self.get_logger().info("Spike Detected at lin_accel_z.")
+                if not self.in_spike_statez:
+                    self.get_logger().info(
+                        f"Spike Detected at lin_accel_z: {imsg.linear_acceleration.z}",
+                    )
+                    self.in_spike_statez = True
 
         self.pub_imu_.publish(imsg)
 
