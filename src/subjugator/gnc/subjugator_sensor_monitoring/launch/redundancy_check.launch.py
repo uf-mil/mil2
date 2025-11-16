079869b0937ace636da49be8359a0ae54430a049
Mohana <mohanashree.pamidimukkala@gmail.com>
2025-11-16 14:32:34 -0500

    added to launch file

--- src/subjugator/gnc/subjugator_sensor_monitoring/launch/redundancy_check.launch.py
+++ src/subjugator/gnc/subjugator_sensor_monitoring/launch/redundancy_check.launch.py
@@ -1,8 +1,45 @@
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+
+    localization_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("subjugator_localization"),
+                        "launch",
+                        "subjugator_localization.launch.py",
+                    ],
+                ),
+            ],
+        ),
+    )
+
+    # Service call to enable localization
+    enable_localization = TimerAction(
+        period=5.0,  # Wait 2 seconds
+        actions=[
+            ExecuteProcess(
+                cmd=[
+                    "ros2",
+                    "service",
+                    "call",
+                    "/subjugator_localization/enable",
+                    "std_srvs/srv/Empty",
+                    "{}",
+                ],
+                output="screen",
+            ),
+        ],
+    )
+
     return LaunchDescription(
         [
             Node(
@@ -11,5 +48,7 @@ def generate_launch_description():
                 name="redundancy_check_node",
                 output="screen",
             ),
+            localization_launch,
+            enable_localization,
         ],
     )
