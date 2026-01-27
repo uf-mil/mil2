1f3e1ce149be653ce768552cc960882bcc3dcfa9
sgupta1703 <sarthakgupta1703@gmail.com>
2026-01-27 19:37:23 +0000

    added gripper connectivity to the sub.

--- src/subjugator/simulation/subjugator_gazebo/src/GripperControl.cc
+++ src/subjugator/simulation/subjugator_gazebo/src/GripperControl.cc
@@ -17,18 +17,22 @@ GripperControl::~GripperControl()
 void GripperControl::Configure(gz::sim::Entity const &entity, std::shared_ptr<sdf::Element const> const &sdf,
                                gz::sim::EntityComponentManager &ecm, gz::sim::EventManager & /*eventMgr*/)
 {
-    std::cout << "[GripperControl] Configuring..." << std::endl;
+    std::cout << "[GripperControl] Configuring GripperControl Plugin ..." << std::endl;
 
+    // Initialize ROS2 if not already running
     if (!rclcpp::ok())
     {
         rclcpp::init(0, nullptr);
     }
-    this->node_ = std::make_shared<rclcpp::Node>("gripper_control_plugin_node");
 
-    // Subscribe to keyboard presses (std_msgs::msg::String)
+    // Create a ROS2 node for this plugin and subscribe to keyboard keypresses
+    this->node_ = std::make_shared<rclcpp::Node>("gripper_control_plugin_node");
     this->key_sub_ = this->node_->create_subscription<std_msgs::msg::String>(
         "/keyboard/keypress", 10, std::bind(&GripperControl::KeypressCallback, this, std::placeholders::_1));
 
+    std::cout << "[GripperControl] ROS2 node created and subscribed to /keyboard/keypress" << std::endl;
+
+    // Try to obtain model name (from Entity Name component or SDF attribute)
     auto nameComp = ecm.Component<gz::sim::components::Name>(entity);
     if (nameComp)
     {
@@ -42,9 +46,9 @@ void GripperControl::Configure(gz::sim::Entity const &entity, std::shared_ptr<sd
             this->model_name_ = sdf->Get<std::string>("name");
             std::cout << "[GripperControl] Got model name from SDF: " << this->model_name_ << std::endl;
         }
-        catch (...)
+        catch (std::exception const &e)
         {
-            gzerr << "[GripperControl] Failed to get model name from SDF attribute 'name'." << std::endl;
+            gzerr << "[GripperControl] Failed to get model name from SDF attribute 'name': " << e.what() << std::endl;
         }
     }
     else
@@ -52,6 +56,7 @@ void GripperControl::Configure(gz::sim::Entity const &entity, std::shared_ptr<sd
         gzerr << "[GripperControl] WARNING: Could not determine model name; plugin topic may be invalid." << std::endl;
     }
 
+    // Parse SDF parameters for joint name and positions (if provided)
     if (sdf)
     {
         if (sdf->HasElement("joint_name"))
@@ -68,25 +73,47 @@ void GripperControl::Configure(gz::sim::Entity const &entity, std::shared_ptr<sd
         }
     }
 
-    // Build Gazebo joint command topic
+    // Determine world name (for logging) by iterating Worlds
+    ecm.Each<gz::sim::components::World, gz::sim::components::Name>(
+        [&](gz::sim::Entity const & /*ent*/, gz::sim::components::World const *,
+            gz::sim::components::Name const *name) -> bool
+        {
+            this->world_name_ = name->Data();
+            return false;  // stop after first world found
+        });
+
+    std::cout << "[GripperControl] World Name: " << this->world_name_ << std::endl;
+
+    // Build Gazebo joint command topic and advertise
     if (!this->model_name_.empty())
     {
         // topic pattern: /model/<model_name>/joint/<joint_name>/cmd_pos
         this->topic_name_ = "/model/" + this->model_name_ + "/joint/" + this->joint_name_ + "/cmd_pos";
         std::cout << "[GripperControl] Advertising gz topic: " << this->topic_name_ << std::endl;
         this->gz_pub_ = this->gz_node_.Advertise<gz::msgs::Double>(this->topic_name_);
+        // Small sanity log if advertise failed to produce a valid publisher (depends on gz implementation)
+        if (!this->gz_pub_)  // operator bool available in some Gazebo versions
+        {
+            gzerr << "[GripperControl] Failed to create gz publisher for topic: " << this->topic_name_ << std::endl;
+        }
+    }
+    else
+    {
+        gzerr << "[GripperControl] Topic name left empty because model name is unknown." << std::endl;
     }
 }
 
-void GripperControl::KeypressCallback(std_msgs::msg::String::SharedPtr msg)
+void GripperControl::KeypressCallback(std_msgs::msg::String::SharedPtr const msg)
 {
     if (!msg)
         return;
 
+    std::cout << "[GripperControl] Keypress received: " << msg->data << std::endl;
+
+    // Toggle only on 'u' press (one-shot)
     if (msg->data == "u" && !this->u_pressed_)
     {
         this->u_pressed_ = true;
-        std::cout << "[GripperControl] 'u' pressed (queued toggle)" << std::endl;
     }
 }
 
@@ -95,6 +122,7 @@ void GripperControl::PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::Entity
     if (info.paused)
         return;
 
+    // Let ROS2 handle incoming messages for this node
     rclcpp::spin_some(this->node_);
 
     // If a 'u' press was detected, toggle and publish the new joint command
@@ -123,4 +151,5 @@ void GripperControl::PostUpdate(gz::sim::UpdateInfo const &info, gz::sim::Entity
 
 }  // namespace gripper_control
 
+// Register plugin for Gazebo
 GZ_ADD_PLUGIN(gripper_control::GripperControl, gz::sim::System, gz::sim::ISystemConfigure, gz::sim::ISystemPostUpdate)
