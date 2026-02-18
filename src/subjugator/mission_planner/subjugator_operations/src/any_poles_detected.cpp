e840a0057fdf8d7c65d8063e0a36bd1b847d9264
markprime13 <mark_crawford@live.com>
2026-02-18 13:26:52 -0500

    test change 2

--- src/subjugator/mission_planner/subjugator_operations/src/any_poles_detected.cpp
+++ src/subjugator/mission_planner/subjugator_operations/src/any_poles_detected.cpp
@@ -15,6 +15,7 @@ BT::NodeStatus AnyPolesDetected::tick()
     if (!ctx_ && (!getInput("ctx", ctx_) || !ctx_))
         return BT::NodeStatus::FAILURE;
     double min_conf = 0.30;
+    double min_conf1 = 0.31;
     (void)getInput("min_conf", min_conf);
 
     std::optional<yolo_msgs::msg::DetectionArray> arr;
