762b60a72e8525133a233d0a636cc9deb114588e
Mohana <mohanashree.pamidimukkala@gmail.com>
2026-03-10 15:33:41 -0400

    fixed the pcdoar_controller.hpp

--- src/mil_common/perception/pcl_stack/include/pcl_stack/pcodar_controller.hpp
+++ src/mil_common/perception/pcl_stack/include/pcl_stack/pcodar_controller.hpp
@@ -1,29 +1,30 @@
 #pragma once
 
-#include <dynamic_reconfigure/client.h>
-#include <dynamic_reconfigure/server.h>
-#include <mil_bounds/BoundsConfig.h>
-#include <nav_msgs/Odometry.h>
+// #include <mil_bounds/BoundsConfig.h>
 #include <pcl/filters/passthrough.h>
-#include <roboteq_msgs/Command.h>
-#include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <std_msgs/Float32.h>
-#include <std_srvs/Trigger.h>
-#include <tf2/convert.h>
-#include <tf2_eigen/tf2_eigen.h>
-#include <tf2_msgs/TFMessage.h>
-#include <tf2_ros/transform_listener.h>
 
+#include "nav_msgs/msg/odometry.hpp"
+// #include <roboteq_msgs/Command.h>
 #include <boost/circular_buffer.hpp>
+#include <rclcpp/rclcpp.hpp>
 
-#include "input_cloud_filter.hpp"
-#include "marker_manager.hpp"
-#include "mil_msgs/msg/object_db_query.hpp"
-#include "object_associator.hpp"
-#include "object_detector.hpp"
-#include "object_map.hpp"
-#include "ogrid_manager.hpp"
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <std_msgs/msg/float32.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <tf2/convert.hpp>
+#include <tf2_eigen/tf2_eigen.hpp>
+#include <tf2_msgs/msg/tf_message.hpp>
+#include <tf2_ros/transform_listener.hpp>
+
+// Uncomment later for more than vision stuff
+// #include "input_cloud_filter.hpp"
+// #include "marker_manager.hpp"
+// #include "mil_msgs/msg/object_db_query.hpp"
+// #include "object_associator.hpp"
+// #include "object_detector.hpp"
+// #include "object_map.hpp"
+// #include "ogrid_manager.hpp"
+
 #include "pcodar_types.hpp"
 #include "persistent_cloud_filter.hpp"
 #include "point_cloud_builder.hpp"
@@ -47,7 +48,8 @@ class NodeBase
 
   protected:
     /// Process a database query ROS service
-    bool DBQuery_cb(mil_msgs::ObjectDBQuery::Request& req, mil_msgs::ObjectDBQuery::Response& res);
+    // bool DBQuery_cb(mil_msgs::ObjectDBQuery::Request& req, mil_msgs::ObjectDBQuery::Response& res);
+
     /// Reset PCODAR
     virtual bool Reset(std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res);
     /// Transform
@@ -55,27 +57,36 @@ class NodeBase
                              ros::Duration timeout = ros::Duration(1, 0));
     /// Transform a pointcloud ROS message into a PCL pointcloud in the global frame
     bool transform_point_cloud(sensor_msgs::PointCloud2 const& pcloud2, point_cloud_i& out);
-    virtual bool bounds_update_cb(mil_bounds::BoundsConfig const& config);
+
+    // virtual bool bounds_update_cb(mil_bounds::BoundsConfig const& config);
+
     virtual void ConfigCallback(Config const& config, uint32_t level);
 
   public:
-    std::shared_ptr<ObjectMap> objects_;
+    // std::shared_ptr<ObjectMap> objects_;
 
   protected:
-    ros::NodeHandle nh_;
-    dynamic_reconfigure::Client<mil_bounds::BoundsConfig> bounds_client_;
-    dynamic_reconfigure::Server<Config> config_server_;
+    // ros::NodeHandle nh_;
+    // dynamic_reconfigure::Client<mil_bounds::BoundsConfig> bounds_client_;
+    // dynamic_reconfigure::Server<Config> config_server_;
 
-    ros::ServiceServer modify_classification_service_;
-    ros::ServiceServer reset_service_;
+    // ros::ServiceServer modify_classification_service_;
+    // ros::ServiceServer reset_service_;
 
+    // std::string global_frame_;
+
+    // tf2_ros::Buffer tf_buffer_;
+    // tf2_ros::TransformListener tf_listener;
+
+    Config config_;
     std::string global_frame_;
-
     tf2_ros::Buffer tf_buffer_;
-    tf2_ros::TransformListener tf_listener;
+    tf2_ros::TransformListener tf_listener_;
 
     // Publishers
-    ros::Publisher pub_objects_;
+    // ros::Publisher pub_objects_;
+
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_objects_;
 
     point_cloud_ptr bounds_;
 
@@ -105,17 +116,24 @@ class Node : public NodeBase
     bool Reset(std_srvs::Trigger::Request& req, std_srvs::Trigger::Response& res) override;
 
   private:
-    ros::Publisher pub_pcl_;
+    // ros::Publisher pub_pcl_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_pcl_;
 
     // Subscriber
-    ros::Subscriber pc_sub;
+    // ros::Subscriber pc_sub;
+
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pc_sub_;
+
+    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr reset_service_;  // for reset requests
 
     // Model (It eventually will be object tracker, but for now just detections)
     InputCloudFilter input_cloud_filter_;
     PersistentCloudFilter persistent_cloud_filter_;
     PointCloudCircularBuffer persistent_cloud_builder_;
     ObjectDetector detector_;
-    Associator ass;
+
+    // uncomment later
+    // Associator ass;
 };
 
 }  // namespace pcodar
--- src/mil_common/perception/pcl_stack/include/pcl_stack/pcodar_types.hpp
+++ src/mil_common/perception/pcl_stack/include/pcl_stack/pcodar_types.hpp
@@ -4,16 +4,45 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/search/kdtree.h>
-#include <pcl_stack/PCODARConfig.h>
-
-#include <unordered_map>
+// #include <pcl_stack/PCODARConfig.h>
 
 #include "mil_msgs/msg/perception_object.hpp"
 
 namespace pcodar
 {
-/// Alias for the dynamic reconfigure object used for PCODAR configuration
-using Config = point_cloud_object_detection_and_recognition::PCODARConfig;
+
+struct Config
+{
+    // May need to be re-tuned?
+
+    // Point cloud builder - How many consectuive lidar scans to stakc before processing them together at once.
+    int accumulator_number_persistant_clouds = 10;
+
+    // Filter - radius (meters) for utlier removal
+    double persistant_cloud_filter_radius = 0.5;
+    int persistant_cloud_filter_min_neighbors = 20;
+
+    // Clusterer -max distance between points to be considered as cluster
+    double cluster_tolerance_m = 4.4;
+    int cluster_min_points = 2;  // min points to be considered a cluster
+
+    // Associator
+    double associator_max_distance = 25000.0;  // to match a new obj with existing obj
+    bool associator_forget_unseen = false;     // if true, removes obj from database when no longer visible.
+
+    // Ogrid
+    double ogrid_height_meters = 1000.0;  // size of occupancy grid.
+    double ogrid_width_meters = 1000.0;
+    double ogrid_resolution_meters_per_cell = 0.3;
+    double ogrid_inflation_meters = 2.0;  // inflates for path planning marigin
+
+    // Intensity filter
+    double intensity_filter_min_intensity = 10.0;
+    double intensity_filter_max_intensity = 100.0;
+};
+
+// /// Alias for the dynamic reconfigure object used for PCODAR configuration
+// using Config = point_cloud_object_detection_and_recognition::PCODARConfig;
 
 /// Alias for type of pointcloud used internally
 using point_t = pcl::PointXYZ;
