a88a8857daaea85e2d080fb683f4557d217d5f28
wingdeans <66850754+wingdeans@users.noreply.github.com>
2026-06-06 09:55:55 -0400

    proper pose difference

--- src/subjugator/testing/probably/src/common.cpp
+++ src/subjugator/testing/probably/src/common.cpp
@@ -8,17 +8,23 @@ State subtract_odom(nav_msgs::msg::Odometry &odom_prev,
                     nav_msgs::msg::Odometry &odom_next) {
     State s{};
     auto &pp = odom_prev.pose, &pn = odom_next.pose;
-    s.x = pn.pose.position.x - pp.pose.position.x;
-    s.y = pn.pose.position.y - pp.pose.position.y;
 
+    // phi: yaw_next - yaw_prev
     auto &qp = pp.pose.orientation, &qn = pn.pose.orientation;
     tf2::Quaternion q_prev{qp.x, qp.y, qp.z, qp.w};
     tf2::Quaternion q_next{qn.x, qn.y, qn.z, qn.w};
 
-    tf2::Matrix3x3 mat{q_next - q_prev};
-    double roll, pitch, yaw;
-    mat.getRPY(roll, pitch, yaw);
-    s.phi = yaw;
+    tf2::Matrix3x3 mat_prev{q_prev}, mat_next{q_next};
+    double r, p, yaw_prev, yaw_next;
+    mat_prev.getRPY(r, p, yaw_prev);
+    mat_prev.getRPY(r, p, yaw_next);
+    s.phi = yaw_next - yaw_prev;
+
+    // position: R1^-1 (t2 - t1)
+    double dx = pn.pose.position.x - pp.pose.position.x;
+    double dy = pn.pose.position.y - pp.pose.position.y;
+    s.x = dx * cos(yaw_prev)  + dy * sin(yaw_prev);
+    s.y = dx * -sin(yaw_prev) - dy * cos(yaw_prev);
 
     return s;
 }
