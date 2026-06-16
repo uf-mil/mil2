ab71738a1df3a2d34b7c57447ad34b93a6bc3a68
Joseph Handsome <jgoodman1@ufl.edu>
2026-06-16 15:08:38 -0400

    ethan dropper

--- src/subjugator/drivers/servo_controller/servo_controller/driver.py
+++ src/subjugator/drivers/servo_controller/servo_controller/driver.py
@@ -20,35 +20,39 @@ class Servo_Controller(Node):
         self.srv3 = self.create_service(Servo, "torpedo", self.torpedo_callback)
 
     def dropper_callback(self, request: Servo, response):
-        if request.angle == 1:
-            dev.set_pwm(0, 310)
-            time.sleep(3)
+        # full range of dropper servo seems to be 488-150
+        # should be 150 to load 1st, 200 to load 2nd, 255 for holding, 285 to drop 1st, 350 to drop 2nd
+        dev.set_pwm(0, request.angle)
+        if request.angle == 0:
+            dev.set_pwm(0, 150)
+            input()
+            dev.set_pwm(0, 200)
+            input()
+            dev.set_pwm(0, 255)
+        elif request.angle == 1:
+            dev.set_pwm(0, 285)
+            time.sleep(1)
         elif request.angle == 2:
-            dev.set_pwm(0, 432)
-            time.sleep(3)
-            dev.set_pwm(0, 138)
-            time.sleep(2)
-        else:
-            print("Nonvalid input, please input 1 or 2")
-        # dev.set_pwm(0, 310)
-        # time.sleep(1)
-        # dev.set_pwm(0, 432)
-        # time.sleep(1)
-        # dev.set_pwm(0, 138)
-        # time.sleep(1)
+            dev.set_pwm(0, 350)
+            time.sleep(1)
+            dev.set_pwm(0, 255)
+            time.sleep(1)
         return response
 
     def gripper_callback(self, request: Servo, response):
+        # full servo range is 489-149, note other servo may be different
+        # should be 318 to screw the worm gear on :)
+        # gripper range is 318(open) 210(close)
+        dev.set_pwm(7, request.angle)
+        if request.angle == 1:
+            dev.set_pwm(7, 318)
+        elif request.angle == 2:
+            dev.set_pwm(7, 210)
+        time.sleep(0.5)
+
         return response
 
     def torpedo_callback(self, request: Servo, response):
-        # dev.set_pwm(4, 450)
-        # dev.set_pwm(4, request.angle)
-        # time.sleep(1)
-        # time.sleep(0.5)
-        # dev.set_pwm(4, 300)
-        # dev.set_pwm(4, 450)
-        # time.sleep(1)
         dev.set_pwm(4, request.angle)
         if request.angle == 1:
             dev.set_pwm(4, 270)
