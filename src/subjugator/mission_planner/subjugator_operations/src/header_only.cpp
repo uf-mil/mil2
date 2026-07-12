5e2ce6327eac8f20d1cd90f8196aa6bc591eadeb
wingdeans <66850754+wingdeans@users.noreply.github.com>
2026-07-11 21:16:08 -0700

    fixes

--- src/subjugator/mission_planner/subjugator_operations/src/header_only.cpp
+++ src/subjugator/mission_planner/subjugator_operations/src/header_only.cpp
@@ -1,14 +1,18 @@
 #include "context.hpp"
 #include "count_when_ticked.hpp"
 #include "go_to_pinger.hpp"
+#include "lookup_waypoint.hpp"
 #include "pitch_style.hpp"
+#include "remember_waypoint.hpp"
 #include "roll_style.hpp"
 #include "topic_ticker.hpp"
 #include "yaw_style.hpp"
 
 REGISTER(CountWhenTicked)
 REGISTER(SonarFollower)
+REGISTER(LookupWaypoint)
 REGISTER(PitchStyle)
+REGISTER(RememberWaypoint)
 REGISTER(RollStyle)
 using TopicTickerOdom = TopicTicker<nav_msgs::msg::Odometry>;
 REGISTER(TopicTickerOdom)
