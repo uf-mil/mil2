ce09155b9a77323f3f48479f49ffa0573ec7f023
Mohana <mohanashree.pamidimukkala@gmail.com>
2026-09-02 16:19:30 -0400

    formatting changes

--- src/prop/perception/pcd/launch/filter.launch.py
+++ src/prop/perception/pcd/launch/filter.launch.py
@@ -6,15 +6,15 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    pcd_pkg_dir = get_package_share_directory("pcd")
-    params_file = os.path.join(pcd_pkg_dir, "config", "pcd_params.yaml")
+    pcd_pkg_dir = get_package_share_directory('pcd')
+    params_file = os.path.join(pcd_pkg_dir, 'config', 'pcd_params.yaml')
 
     filter_node = Node(
-        package="pcd",
-        executable="pcl_filter_node",
-        name="pcl_filter",
+        package='pcd',
+        executable='pcl_filter_node',
+        name='pcl_filter',
         parameters=[params_file],
-        output="screen",
+        output='screen',
     )
 
     return LaunchDescription([filter_node])
